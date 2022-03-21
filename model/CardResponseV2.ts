5304422c80bf84b5041f4cae35549bd79386a3d5
ChriJ <christofferj93@gmail.com>
2022-03-21 17:13:28 +0100

    Make this type require fields. (#32)
    Co-authored-by: Christoffer Johansson <christoffer@issuefab.com>

--- model/CardResponseV2.ts
+++ model/CardResponseV2.ts
@@ -4,36 +4,36 @@ import { CardUsage } from "./CardUsage"
 import { ProviderCode } from "./ProviderCode"
 
 export interface CardResponseV2 {
-	cardType?: CardTypeSpecification | string
-	cardNumber?: string
-	cvv?: string
-	expiryDate?: string
-	nameOnCard?: string
-	balance?: number
-	issueDate?: string
-	providerCode?: ProviderCode
-	providerCardId?: string
-	usage?: CardUsage
-	fundingAccount?: CardFundingAccountResponse
-	createdBy?: string
+	cardType: CardTypeSpecification | string
+	cardNumber: string
+	cvv: string
+	expiryDate: string
+	nameOnCard: string
+	balance: number
+	issueDate: string
+	providerCode: ProviderCode
+	providerCardId: string
+	usage: CardUsage
+	fundingAccount: CardFundingAccountResponse
+	createdBy: string
 }
 
 export namespace CardResponseV2 {
 	export function is(value: CardResponseV2 | any): value is CardResponseV2 {
 		return (
 			typeof value == "object" &&
-			(value.cardType == undefined || typeof value.cardType == "string" || CardTypeSpecification.is(value.cardType)) &&
-			(value.cardNumber == undefined || typeof value.cardNumber == "string") &&
-			(value.cvv == undefined || typeof value.cvv == "string") &&
-			(value.expiryDate == undefined || typeof value.expiryDate == "string") &&
-			(value.nameOnCard == undefined || typeof value.nameOnCard == "string") &&
-			(value.balance == undefined || typeof value.balance == "number") &&
-			(value.issueDate == undefined || typeof value.issueDate == "string") &&
-			(value.providerCode == undefined || ProviderCode.is(value.providerCode)) &&
-			(value.providerCardId == undefined || typeof value.providerCardId == "string") &&
-			(value.usage == undefined || CardUsage.is(value.usage)) &&
-			(value.fundingAccount == undefined || CardFundingAccountResponse.is(value.fundingAccount)) &&
-			(value.createdBy == undefined || typeof value.createdBy == "string")
+			(typeof value.cardType == "string" || CardTypeSpecification.is(value.cardType)) &&
+			typeof value.cardNumber == "string" &&
+			typeof value.cvv == "string" &&
+			typeof value.expiryDate == "string" &&
+			typeof value.nameOnCard == "string" &&
+			typeof value.balance == "number" &&
+			typeof value.issueDate == "string" &&
+			ProviderCode.is(value.providerCode) &&
+			typeof value.providerCardId == "string" &&
+			CardUsage.is(value.usage) &&
+			CardFundingAccountResponse.is(value.fundingAccount) &&
+			typeof value.createdBy == "string"
 		)
 	}
 }
