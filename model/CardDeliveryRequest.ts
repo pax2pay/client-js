172e31e8079f47d3c9ca66983e7dc2f4a04c9a87
qijieye <86956109+qijieye@users.noreply.github.com>
2022-03-30 15:02:28 +0200

    Test (#38)
    * for test.
    
    * for test
    
    * make message and linkExpiry optional.

--- model/CardDeliveryRequest.ts
+++ model/CardDeliveryRequest.ts
@@ -1,7 +1,7 @@
 import * as isoly from "isoly"
 
 export interface CardDeliveryRequest {
-	to: Set<string>
-	message: string
-	linkExpiry: isoly.Date
+	to: string | [string, string]
+	message?: string
+	linkExpiry?: isoly.Date
 }
