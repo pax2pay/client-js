c1ff9d97a462b4849c1b0558a4812e079980fc75
Qijie Yu <qijie@issuefab.com>
2022-05-23 13:16:50 +0200

    revert last commit.

--- model/BookingInfoResponse.ts
+++ model/BookingInfoResponse.ts
@@ -1,11 +1,9 @@
-import { BookingInfo } from "./BookingInfo"
 import { FiveFieldsBookingInfoResponse } from "./FiveFieldsBookingInfoResponse"
 import { FlightBookingInfoResponse } from "./FlightBookingInfoResponse"
 import { HotelBookingInfoResponse } from "./HotelBookingInfoResponse"
 import { InvoiceBookingInfoResponse } from "./InvoiceBookingInfoResponse"
 
 export type BookingInfoResponse =
-	| BookingInfo
 	| FiveFieldsBookingInfoResponse
 	| HotelBookingInfoResponse
 	| InvoiceBookingInfoResponse
