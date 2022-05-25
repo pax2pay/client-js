d64658e4c811b86f1e0aefcbba981b8e45d4fec4
Qijie Yu <qijie@issuefab.com>
2022-05-25 17:33:16 +0200

    updated.

--- Client/User/index.ts
+++ Client/User/index.ts
@@ -6,9 +6,4 @@ export class User extends Resource<model.UserResponse, model.UserResponse> {
 	constructor(connection: Connection, folder: string, backend: model.UserResponse) {
 		super(connection, folder, backend)
 	}
-	async getUsersActiveRoles(username: string, token: string): Promise<string[] | model.ErrorResponse> {
-		this.connection.token = token
-		const result = await this.connection.get<string[]>(`users/${username}/roles/minified`)
-		return result
-	}
 }
