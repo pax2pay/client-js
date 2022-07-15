d49fe46020337769640697f2361ac09f656884d9
Qijie Yu <qijie@issuefab.com>
2022-07-15 10:16:47 +0200

    Added is function.

--- model/CardTypeResponse.ts
+++ model/CardTypeResponse.ts
@@ -3,3 +3,13 @@ export interface CardTypeResponse {
 	representAs: string
 	cardName?: string
 }
+export namespace CardTypeResponse {
+	export function is(value: CardTypeResponse | any): value is CardTypeResponse {
+		return (
+			typeof value == "object" &&
+			typeof value.cardType == "string" &&
+			typeof value.representAs == "string" &&
+			typeof value.cardName == "string"
+		)
+	}
+}
