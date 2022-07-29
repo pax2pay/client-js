b731c5fd91dce27cd1ed64610042c8858e1ceb14
Qijie Yu <qijie@issuefab.com>
2022-07-29 13:28:29 +0200

    added interface for password reset.

--- Client/Users/index.ts
+++ Client/Users/index.ts
@@ -40,6 +40,10 @@ export class Users extends Collection<model.UserResponse, model.UserSearchReques
 		const result = await this.connection.get<string[]>(`users/${username}/roles/minified`)
 		return result
 	}
+	async resetPassword(username: string): Promise<model.PasswordResetResponse | model.ErrorResponse> {
+		const result = await this.connection.post<model.PasswordResetResponse>(`auth/passwordreset`, { username: username })
+		return result
+	}
 	async updateRolesetOnUser(username: string, roleset: string): Promise<model.UserRoleResponse | model.ErrorResponse> {
 		const result = await this.connection.put<model.UserRoleResponse>(`users/${username}/rolesets/${roleset}`, undefined)
 		return result
---
+++ model/PasswordResetResponse.ts
@@ -0,0 +1,4 @@
+export interface PasswordResetResponse {
+	status: string
+	url: string
+}
