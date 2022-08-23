80d7ca5ac98fb43c4b46770bc9c17215e8881425
ChriJ <christofferj93@gmail.com>
2022-08-23 10:23:52 +0200

    type checking for CardResponse (#68)

--- model/AgentBookingInfo.ts
+++ model/AgentBookingInfo.ts
@@ -7,3 +7,15 @@ export interface AgentBookingInfo {
 	agentBookingBasketRef?: string
 	agentBookingBookingRef?: string
 }
+
+export namespace AgentBookingInfo {
+	export function is(value: AgentBookingInfo | any): value is AgentBookingInfo {
+		return (
+			typeof value == "object" &&
+			(value.agentBookingOrgCode == undefined || typeof value.agentBookingOrgCode == "string") &&
+			(value.agentBookingSystemRef == undefined || typeof value.agentBookingSystemRef == "string") &&
+			(value.agentBookingBasketRef == undefined || typeof value.agentBookingBasketRef == "string") &&
+			(value.agentBookingBookingRef == undefined || typeof value.agentBookingBookingRef == "string")
+		)
+	}
+}
--- model/TravelPartyInfo.ts
+++ model/TravelPartyInfo.ts
@@ -7,3 +7,15 @@ export interface TravelPartyInfo {
 	travelPartyNumChild?: number
 	travelPartyNumInfant?: number
 }
+
+export namespace TravelPartyInfo {
+	export function is(value: TravelPartyInfo | any): value is TravelPartyInfo {
+		return (
+			typeof value == "object" &&
+			(value.travelPartyLeadPax == undefined || typeof value.travelPartyLeadPax == "string") &&
+			(value.travelPartyNumAdult == undefined || typeof value.travelPartyNumAdult == "number") &&
+			(value.travelPartyNumChild == undefined || typeof value.travelPartyNumChild == "number") &&
+			(value.travelPartyNumInfant == undefined || typeof value.travelPartyNumInfant == "number")
+		)
+	}
+}
