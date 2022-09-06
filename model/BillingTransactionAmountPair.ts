33fa6e3780640c246b6c24a32b57786610c62ae1
Qijie Yu <qijie@issuefab.com>
2022-09-06 09:34:24 +0200

    updated the interface of  BillTransactionAmountPair.

--- model/BillingTransactionAmountPair.ts
+++ model/BillingTransactionAmountPair.ts
@@ -4,6 +4,8 @@ export interface BillingTransactionAmountPair {
 	billing: AmountPair
 	transaction: AmountPair
 	fxRate: number
+	rebate?: number
+	rebateRate?: number
 }
 export namespace BillingTransactionAmountPair {
 	export function is(value: BillingTransactionAmountPair | any): value is BillingTransactionAmountPair {
@@ -11,7 +13,9 @@ export namespace BillingTransactionAmountPair {
 			typeof value == "object" &&
 			AmountPair.is(value.billing) &&
 			AmountPair.is(value.transaction) &&
-			typeof value.fxRate == "number"
+			typeof value.fxRate == "number" &&
+			(typeof value.rebate == "number" || value.rebate == undefined) &&
+			(typeof value.rebateRate == "number" || value.rebateRate == undefined)
 		)
 	}
 }
