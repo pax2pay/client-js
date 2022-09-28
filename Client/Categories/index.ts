b04ad2517f63e958f5d5134f26a0ce512184bee4
ChriJ <christofferj93@gmail.com>
2022-09-28 09:46:23 +0200

    Added forgotten response extraction to endpoint

--- Client/Categories/index.ts
+++ Client/Categories/index.ts
@@ -1,3 +1,4 @@
+import * as model from "../../model"
 import { CategoryResponse } from "../../model/CategoryResponse"
 import { Connection } from "../Connection"
 export class Categories {
@@ -7,7 +8,10 @@ export class Categories {
 		return new Categories(connection)
 	}
 	async getAllCategories() {
-		const result = await this.connection.get<CategoryResponse[]>(`category?size=100`)
-		return result
+		const result = await this.connection.get<{ list: CategoryResponse[]; totalCount: number }>(`category?size=100`)
+		if (!model.ErrorResponse.is(result))
+			return result.list
+		else
+			return result
 	}
 }
