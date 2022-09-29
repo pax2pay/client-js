f52dc5f0f2cb4a7e7e46bb45077b616a1a26f6cc
ChriJ <christofferj93@gmail.com>
2022-09-29 14:34:08 +0200

    ui#409 - User Update: user roles and category can't update

--- model/UserChangeRequest.ts
+++ model/UserChangeRequest.ts
@@ -8,4 +8,21 @@ export interface UserChangeRequest {
 	category?: string
 	status?: UserStatus
 	userLimits?: UserLimitsRequest[]
+	removeCategory?: boolean
+}
+
+export namespace UserChangeRequest {
+	export function is(value: UserChangeRequest | any): value is UserChangeRequest {
+		return (
+			typeof value == "object" &&
+			typeof value.firstName == "string" &&
+			typeof value.lastName == "string" &&
+			typeof value.email == "string" &&
+			(value.category == undefined || typeof value.category == "string") &&
+			(value.status == undefined || UserStatus.is(value.status)) &&
+			(value.userLimits == undefined ||
+				(Array.isArray(value.userLimits) && value.userLimits.every((item: any) => UserLimitsRequest.is(item)))) &&
+			(value.removeCategory == undefined || typeof value.removeCategory == "boolean")
+		)
+	}
 }
