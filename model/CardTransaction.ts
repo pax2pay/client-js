5f658bee78c02e7cbe13c3093185dd4aebd2afeb
ChriJ <christofferj93@gmail.com>
2022-10-06 14:58:06 +0200

    ui#477 - Authorisations etc don't show amount (#87)

--- model/CardTransaction.ts
+++ model/CardTransaction.ts
@@ -23,3 +23,29 @@ export interface CardTransaction {
 	friendlyName?: string
 	expectedFundingTimestamp?: isoly.DateTime
 }
+
+export namespace CardTransaction {
+	export function is(value: CardTransaction | any): value is CardTransaction {
+		return (
+			typeof value == "object" &&
+			typeof value.id == "number" &&
+			typeof value.providerTransactionId == "string" &&
+			typeof value.organisationCode == "string" &&
+			typeof value.organisationName == "string" &&
+			TransactionType.is(value.transactionType) &&
+			ProviderCode.is(value.provider) &&
+			typeof value.prvTimestamp == "string" &&
+			typeof value.associatedAccount == "number" &&
+			typeof value.issuedAmount == "number" &&
+			Currency.is(value.issuedCurrency) &&
+			typeof value.createdOn == "string" &&
+			typeof value.createdBy == "string" &&
+			(value.groupId == undefined || typeof value.groupId == "number") &&
+			(value.exchangeRate == undefined || typeof value.exchangeRate == "number") &&
+			(value.description == undefined || typeof value.description == "string") &&
+			(value.bookingInfo == undefined || BookingInfo.is(value.bookingInfo)) &&
+			(value.friendlyName == undefined || typeof value.friendlyName == "string") &&
+			(value.expectedFundingTimestamp == undefined || isoly.DateTime.is(value.expectedFundingTimestamp))
+		)
+	}
+}
--- model/TransactionType.ts
+++ model/TransactionType.ts
@@ -1,16 +1,25 @@
-export type TransactionType =
-	| "CREATE_CARD"
-	| "AUTHORISATION"
-	| "SETTLEMENT"
-	| "REVERSAL"
-	| "DEPOSIT"
-	| "FUND_CARD"
-	| "PURCHASE"
-	| "REFUND"
-	| "SWEEP"
-	| "CLOSE"
-	| "FREEZE"
-	| "THAW"
-	| "APPROVAL_PENDING"
-	| "EXPIRE"
-	| "UNKNOWN"
+const transactionType = [
+	"CREATE_CARD",
+	"AUTHORISATION",
+	"SETTLEMENT",
+	"REVERSAL",
+	"DEPOSIT",
+	"FUND_CARD",
+	"PURCHASE",
+	"REFUND",
+	"SWEEP",
+	"CLOSE",
+	"FREEZE",
+	"THAW",
+	"APPROVAL_PENDING",
+	"EXPIRE",
+	"UNKNOWN",
+] as const
+
+export type TransactionType = typeof transactionType[number]
+
+export namespace TransactionType {
+	export function is(value: TransactionType | any): value is TransactionType {
+		return typeof value == "string" && transactionType.includes(value as TransactionType)
+	}
+}
