80078644f521623f1872f4053fa4dfc9cec13cbf
ChriJ <christofferj93@gmail.com>
2022-10-06 14:32:15 +0200

    ui#477 - Authorisations etc don't show amount

--- model/CardProcessedTransaction.ts
+++ model/CardProcessedTransaction.ts
@@ -18,3 +18,24 @@ export interface CardProcessedTransaction {
 		orderId?: string
 	}
 }
+
+export namespace CardProcessedTransaction {
+	export function is(value: CardProcessedTransaction | any): value is CardProcessedTransaction {
+		return (
+			typeof value == "object" &&
+			isoly.DateTime.is(value.timestamp) &&
+			ProviderCode.is(value.provider) &&
+			typeof value.description == "string" &&
+			TransactionType.is(value.type) &&
+			(value.reference == undefined || typeof value.reference == "string") &&
+			(value.status == undefined || TransactionStatus.is(value.status)) &&
+			(value.balance == undefined || typeof value.balance == "number") &&
+			(value.fundsChanged == undefined || typeof value.fundsChanged == "number") &&
+			(value.currency == undefined || isoly.Currency.is(value.currency)) &&
+			(value.reason == undefined || typeof value.reason == "string") &&
+			(value.additionalInformation == undefined ||
+				(typeof value.additionalInformation == "object" &&
+					(value.orderId == undefined || typeof value.orderId == "string")))
+		)
+	}
+}
