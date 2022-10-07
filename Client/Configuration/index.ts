432eb037d1387aa96904514ed61a77841b68f646
qijieye <86956109+qijieye@users.noreply.github.com>
2022-10-07 10:26:31 +0200

    ui#464 - retrieve organisation config to get config default value if present (#88)
    * added specific method for organisation config.
    
    * added getUserConfig.

--- Client/Configuration/index.ts
+++ Client/Configuration/index.ts
@@ -11,8 +11,12 @@ export class Configuration {
 		const result = await this.connection.get<Currency[]>(`config/currencies`)
 		return result
 	}
-	async getConfigurationFor(type: string): Promise<model.OrganisationConfig | model.ErrorResponse> {
-		const result = await this.connection.get<model.OrganisationConfig>(`config/${type}`)
+	async getOrganisationConfig(): Promise<model.OrganisationConfig | model.ErrorResponse> {
+		const result = await this.connection.get<model.OrganisationConfig>(`config/organisation`)
+		return result
+	}
+	async getUserConfig(): Promise<model.UserConfig | model.ErrorResponse> {
+		const result = await this.connection.get<model.UserConfig>(`config/user`)
 		return result
 	}
 }
