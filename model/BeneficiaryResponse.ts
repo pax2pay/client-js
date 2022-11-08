1d89bff7f985cbb3901ccc1952191fccbab36ecb
Qijie Yu <qijie@issuefab.com>
2022-11-08 09:14:41 +0100

    providerBeneficiaryId and providerCode are not needed in BeneficiaryResponse.

--- model/BeneficiaryResponse.ts
+++ model/BeneficiaryResponse.ts
@@ -1,5 +1,4 @@
 import { BeneficiaryStatus } from "./BeneficiaryStatus"
-import { ProviderCode } from "./ProviderCode"
 import { TransferDestinationInfo } from "./TransferDestinationInfo"
 
 export interface BeneficiaryResponse {
@@ -8,8 +7,6 @@ export interface BeneficiaryResponse {
 	status?: BeneficiaryStatus
 	name?: string
 	fullName?: string
-	providerBeneficiaryId?: string
-	providerCode?: ProviderCode
 	beneficiaryId?: string
 	createdOn?: string
 	history?: BeneficiaryResponse[]
@@ -26,8 +23,6 @@ export namespace BeneficiaryResponse {
 			(value.fullName == undefined || typeof value.fullName == "string") &&
 			(value.beneficiaryId == undefined || typeof value.beneficiaryId == "string") &&
 			(value.createdOn == undefined || typeof value.createdOn == "string") &&
-			(value.providerBeneficiaryId == undefined || typeof value.providerBeneficiaryId == "string") &&
-			(value.providerCode == undefined || ProviderCode.is(value.providerCode)) &&
 			(value.history == undefined || Array.isArray(value.history)) //not checking same type because of risk of being slow
 		)
 	}
