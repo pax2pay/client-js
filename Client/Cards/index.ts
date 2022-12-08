575593530e88fcaa635c454656b4abfcd1b1d142
Qijie Yu <qijie@issuefab.com>
2022-12-08 17:24:56 +0100

    added catch error.

--- Client/Cards/index.ts
+++ Client/Cards/index.ts
@@ -39,10 +39,12 @@ export class Cards extends List<
 				type: "application/json",
 			})
 		)
-		Promise.all([addFile, addRequest]).then(async () => {
-			const result = await this.connection.post<model.CardResponseV2>("v2/cards/virtual", formData)
-			return model.ErrorResponse.is(result) ? result : this.map(result)
-		})
+		Promise.all([addFile, addRequest])
+			.then(async () => {
+				const result = await this.connection.post<model.CardResponseV2>("v2/cards/virtual", formData)
+				return model.ErrorResponse.is(result) ? result : this.map(result)
+			})
+			.catch(error => console.error(error.message))
 	}
 	async createLegacy(request: model.CreateCardRequest) {
 		const result = await this.connection.post<model.CardResponse>("cards/virtual", request)
