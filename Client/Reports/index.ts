871fb66c7741be1ad6bd3918d815a7f1fe57d495
qijieye <86956109+qijieye@users.noreply.github.com>
2023-02-07 16:49:50 +0100

    ui#897 - Download user report (#125)
    * ui #897 - download user report.
    
    * reuse existing type.
    
    * clean up code.
    
    * correct type.

--- Client/Reports/index.ts
+++ Client/Reports/index.ts
@@ -68,6 +68,10 @@ export class Reports {
 		const result = await this.connection.post<model.StatementReportUrlResponse>(`statement/download`, request)
 		return result
 	}
+	async getUserReportUrl(request: model.StatementReportUrlRequest) {
+		const result = await this.connection.post<model.StatementReportUrlResponse>(`reports/user/download`, request)
+		return result
+	}
 	static create(connection: Connection) {
 		return new Reports(connection)
 	}
