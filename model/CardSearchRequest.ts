e0636737e118dd8e9dc443d3925c902baa541c53
qijieye <86956109+qijieye@users.noreply.github.com>
2023-02-27 11:22:02 +0100

    ui#913 - Additional filter ability in payment room: correct usage type. (#128)

--- model/CardSearchRequest.ts
+++ model/CardSearchRequest.ts
@@ -24,7 +24,7 @@ export interface CardSearchRequest {
 	cardNumber?: string
 	providerAccountId?: string
 	createdOn?: Range<DateTime>
-	usage?: CardUsage
+	usage?: CardUsage[]
 	bookingInfoText?: string
 	bookingInfoType?: (BookingInfoType | "NONE")[]
 }
