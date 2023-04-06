1c179122ba15866799a51403490fcad3b30faa8d
ChriJ <christofferj93@gmail.com>
2023-04-06 14:27:54 +0200

    ui#988 - Enable setting default card type and funding account in organisation setting - client changes (#140)

--- model/OrganisationConfig.ts
+++ model/OrganisationConfig.ts
@@ -1,7 +1,9 @@
+import { Currency } from "isoly"
 import { CardDeliveryEmailConfig } from "./CardDeliveryEmailConfig"
 import { CardTypesConfig } from "./CardTypesConfig"
 import { FundingAccountInboundTransferNotificationConfig } from "./FundingAccountInboundTransferNotificationConfig"
 import { FundingLimitConfig } from "./FundingLimitConfig"
+import { ProviderCode } from "./ProviderCode"
 /**
  * Organisation config, optional
  */
@@ -9,6 +11,9 @@ export interface OrganisationConfig {
 	showDefaultRolesets?: boolean
 	defaultModulrUsage?: "SINGLE_USE" | "SINGLE_USE_ALLOW_TEST_AUTH" | "MULTIPLE_USE"
 	defaultExpiryMonthDelta?: number
+	defaultExpiryMonthDeltaPerCurrency?: Partial<Record<Currency, number>>
+	defaultPortalCardType?: Partial<Record<ProviderCode, string>>
+	defaultPortalFundingAccount?: Partial<Record<ProviderCode, string>>
 	cardTypes?: CardTypesConfig
 	inboundTransferConfig?: FundingAccountInboundTransferNotificationConfig
 	fundingLimitConfig?: FundingLimitConfig
