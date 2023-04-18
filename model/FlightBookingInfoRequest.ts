045707907ee3bd9b9950d46e225bdf0f3d54c777
qijieye <86956109+qijieye@users.noreply.github.com>
2023-04-18 12:08:02 +0200

    ui#989 - Add new metadata type in card creation (#145)
    * ui#989 - Add new metadata type in card creation
    
    * updated.
    
    * updated.

--- model/FlightBookingInfoRequest.ts
+++ model/FlightBookingInfoRequest.ts
@@ -4,10 +4,25 @@ import { Passengers } from "./Passengers"
 import { References } from "./References"
 
 export interface FlightBookingInfoRequest {
-	passengers?: Passengers
-	flight?: FlightInfo
-	references?: References
+	type: "FLIGHT"
+	passengers: Passengers
+	flight: FlightInfo
+	references: References
 	cost?: number
 	currency?: isoly.Currency
-	timestamp?: isoly.DateTime
+	timestamp: isoly.DateTime
+}
+export namespace FlightBookingInfoRequest {
+	export function is(value: FlightBookingInfoRequest | any): value is FlightBookingInfoRequest {
+		return (
+			typeof value == "object" &&
+			value.type == "FLIGHT" &&
+			Passengers.is(value.passengers) &&
+			FlightInfo.is(value.flight) &&
+			References.is(value.references) &&
+			(value.cost == undefined || typeof value.cost == "number") &&
+			(value.currency == undefined || isoly.Currency.is(value.currency)) &&
+			isoly.DateTime.is(value.timestamp)
+		)
+	}
 }
