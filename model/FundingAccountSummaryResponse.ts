93b552c17806cf3aaf7bc9b353b10780b88602a7
Qijie Yu <qijie@issuefab.com>
2023-09-21 17:10:36 +0200

    add friendly name in funding account summary response

--- model/FundingAccountSummaryResponse.ts
+++ model/FundingAccountSummaryResponse.ts
@@ -6,6 +6,11 @@ export interface FundingAccountSummaryResponse {
 
 export namespace FundingAccountSummaryResponse {
 	export function is(value: FundingAccountSummaryResponse | any): value is FundingAccountSummaryResponse {
-		return typeof value == "object" && typeof value.providerAccountId == "string" && typeof value.balance == "number"
+		return (
+			typeof value == "object" &&
+			typeof value.providerAccountId == "string" &&
+			typeof value.balance == "number" &&
+			typeof value.friendlyName == "string"
+		)
 	}
 }
