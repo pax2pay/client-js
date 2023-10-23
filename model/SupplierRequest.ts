1f01872745645e44b4a66b8797c5617eb452a245
Alf Håkansson <alf.hakansson@pax2pay.com>
2023-10-23 13:31:05 +0200

    Added is function for OmnisetupProviderRequest

--- model/SupplierRequest.ts
+++ model/SupplierRequest.ts
@@ -6,3 +6,15 @@ export interface SupplierRequest {
 	status: "ACTIVE" | "DELETED"
 	codes: string[]
 }
+
+export namespace SupplierRequest {
+	export function is(value: SupplierRequest): value is SupplierRequest {
+		return (
+			ProductType.is(value.productType) &&
+			typeof value.name == "string" &&
+			(value.status == "ACTIVE" || value.status == "DELETED") &&
+			Array.isArray(value.codes) &&
+			value.codes.every(c => typeof c == "string")
+		)
+	}
+}
