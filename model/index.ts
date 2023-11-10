40a89db35b7707ec17852279666a92b9eaf34a80
Alf Håkansson <alf.hakansson@pax2pay.com>
2023-11-10 14:24:00 +0100

    Added is functions on omniSetup objects. Added CardUsage to model

--- model/index.ts
+++ model/index.ts
@@ -52,6 +52,7 @@ import { CardTypesConfig } from "./CardTypesConfig"
 import { CardTypeSearchRequest } from "./CardTypeSearchRequest"
 import { CardTypeSpecification } from "./CardTypeSpecification"
 import { CardTypeSpecificationFlag } from "./CardTypeSpecificationFlag"
+import { CardUsage } from "./CardUsage"
 import { CategoryFundingAccountAccessRequest } from "./CategoryFundingAccountAccessRequest"
 import { CategoryLimitResponse } from "./CategoryLimitResponse"
 import { CategoryResponse } from "./CategoryResponse"
@@ -259,6 +260,7 @@ export {
 	CardTypeSearchRequest,
 	CardTypeSpecification,
 	CardTypeSpecificationFlag,
+	CardUsage,
 	CategoryFundingAccountAccessRequest,
 	UpdateCategoryRequest,
 	CategoryLimitResponse,
