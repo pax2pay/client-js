17b7ea476e328048032e9f483185ead2b15ab4cb
qijieye <86956109+qijieye@users.noreply.github.com>
2023-11-16 11:53:53 +0100

    ui#1344 - enable/disable 2fa enforcement in organization setting (#186)
    * ui#1344 - enable/disable 2fa enforcement in organization setting
    
    * update user config
    
    * securityConfig should be optional
    
    * enforce2fa should be optional

--- model/UserConfig.ts
+++ model/UserConfig.ts
@@ -1,5 +1,7 @@
 import { CardTypesConfig } from "./CardTypesConfig"
+import { SecurityConfig } from "./SecurityConfig"
 
 export interface UserConfig {
 	cardTypes?: CardTypesConfig
+	securityConfig?: SecurityConfig
 }
