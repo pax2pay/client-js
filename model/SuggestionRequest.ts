b15f18e473dfc902aa41aca8c48e06efa37c4122
Qijie Yu <qijie@issuefab.com>
2023-12-04 17:18:50 +0100

    add optional meta in request

--- model/SuggestionRequest.ts
+++ model/SuggestionRequest.ts
@@ -2,11 +2,13 @@ import { Currency } from "isoly"
 import { SuggestionCardPaymentMethodRequest } from "./SuggestionCardPaymentMethodRequest"
 import { SuggestionMerchantRequest } from "./SuggestionMerchantRequest"
 import { SuggestionPaymentMethodRequest } from "./SuggestionPaymentMethodRequest"
+import { BookingInfoRequest } from "./BookingInfoRequest"
 
 export interface SuggestionRequest {
 	merchant?: SuggestionMerchantRequest
 	amount: number
 	currency: Currency
+	meta?: BookingInfoRequest
 	paymentMethod?: SuggestionCardPaymentMethodRequest //Use the specific types here, add transfer when available
 }
 
