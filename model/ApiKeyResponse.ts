38485b97dcbb701baef2938113522dd2622d67bc
Qijie Yu <qijie@issuefab.com>
2024-03-11 15:19:17 +0100

    correct type

--- model/ApiKeyResponse.ts
+++ model/ApiKeyResponse.ts
@@ -1,12 +1,11 @@
 import { DateTime } from "isoly"
-import { Range } from "./Range"
 
 export interface ApiKeyResponse {
 	identifier: string
 	displayName: string
 	rolesets?: string[]
 	roles?: string[]
-	lastKeyUsage: Range<DateTime>
+	lastKeyUsage: DateTime
 	active?: boolean
 	createdBy: string
 }
