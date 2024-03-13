329776d29af9dfcb292510e5ef9bfdd46ad897f0
Qijie Yu <qijie@issuefab.com>
2024-03-13 16:33:56 +0100

    ui#1576 - API keys - front end - update searchRolesetsRequest

--- model/SearchRolesetsRequest.ts
+++ model/SearchRolesetsRequest.ts
@@ -1,10 +1,10 @@
 export interface SearchRolesetsRequest {
 	name?: string
 	description?: string
-	includeDefaults: "INCLUDE" | "EXCLUDE" | "ONLY"
+	includeDefaults?: "INCLUDE" | "EXCLUDE" | "ONLY"
 	containsRoles?: string[]
 	doesNotContainRoles?: string[]
-	includeInternal: "INCLUDE" | "EXCLUDE" | "ONLY"
-	includeShared: "INCLUDE" | "EXCLUDE" | "ONLY"
+	includeInternal?: "INCLUDE" | "EXCLUDE" | "ONLY"
+	includeShared?: "INCLUDE" | "EXCLUDE" | "ONLY"
 	appliesTo?: "USER" | "API_KEY"
 }
