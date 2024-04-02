30a7f6c7925f64874c359aa6a33b08bbff675b5b
Magnus Gustafsson <magnus.gustafsson@pax2pay.com>
2024-04-02 12:54:47 +0200

    ui#1688 - Add providerCode to card type specification

--- model/ProviderCode.ts
+++ model/ProviderCode.ts
@@ -1,8 +1,9 @@
-const providerCodes = ["conferma", "ixaris", "wex", "fake", "lodged", "modulr", "unknown", "pax2pay"] as const
-export type ProviderCode = typeof providerCodes[number]
+import { isly } from "isly"
+
+export type ProviderCode = typeof ProviderCode.types[number]
 
 export namespace ProviderCode {
-	export function is(value: unknown): value is ProviderCode {
-		return typeof value == "string" && providerCodes.includes(value as ProviderCode)
-	}
+	export const types = ["conferma", "ixaris", "wex", "fake", "lodged", "modulr", "unknown", "pax2pay"] as const
+	export const type = isly.string(types)
+	export const is = type.is
 }
