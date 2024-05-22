8babe23b2383b180fd2bf33980cc06a69e3dcf24
qijieye <86956109+qijieye@users.noreply.github.com>
2024-05-22 15:08:27 +0200

    ui#1832 - Organisation create - bank transfer option (#220)
    * ui#1832 - Organisation create - bank transfer option
    
    * update
    
    * requested changes

--- Client/Configuration/index.ts
+++ Client/Configuration/index.ts
@@ -11,18 +11,30 @@ export class Configuration {
 	async getAvailableCurrency(providerCode: ProviderCode = "modulr"): Promise<Currency[] | model.ErrorResponse> {
 		return await this.connection.get<Currency[]>(`${this.folder}/currencies`, { provider: providerCode })
 	}
-	async updateOrganisationConfig(
-		request: model.OrganisationConfig
-	): Promise<model.OrganisationConfig | model.ErrorResponse> {
-		return await this.connection.post<model.OrganisationConfig>(`${this.folder}/organisation`, request)
-	}
-	async getOrganisationConfig(): Promise<model.OrganisationConfig | model.ErrorResponse> {
+	async getOrganisation(): Promise<model.OrganisationConfig | model.ErrorResponse> {
 		return await this.connection.get<model.OrganisationConfig>(`${this.folder}/organisation`)
 	}
-	async getUserConfig(): Promise<model.UserConfig | model.ErrorResponse> {
+	async updateOrganisation(request: model.OrganisationConfig): Promise<model.OrganisationConfig | model.ErrorResponse> {
+		return await this.connection.post<model.OrganisationConfig>(`${this.folder}/organisation`, request)
+	}
+	async getInternalOrganisation(organisation: string): Promise<model.InternalOrganisationConfig | model.ErrorResponse> {
+		return await this.connection.get<model.InternalOrganisationConfig>(
+			`${this.folder}/organisation_internal/${organisation}`
+		)
+	}
+	async updateInternalOrganisation(
+		organisation: string,
+		request: model.InternalOrganisationConfig
+	): Promise<model.InternalOrganisationConfig | model.ErrorResponse> {
+		return await this.connection.post<model.InternalOrganisationConfig>(
+			`${this.folder}/organisation_internal/${organisation}`,
+			request
+		)
+	}
+	async getUser(): Promise<model.UserConfig | model.ErrorResponse> {
 		return await this.connection.get<model.UserConfig>(`${this.folder}/user`)
 	}
-	async updateUserConfig(request: model.UserConfig): Promise<model.UserConfig | model.ErrorResponse> {
+	async updateUser(request: model.UserConfig): Promise<model.UserConfig | model.ErrorResponse> {
 		return await this.connection.post<model.UserConfig>(`${this.folder}/user`, request)
 	}
 	async getPortalFeatures(): Promise<model.PaxpayFeature[] | model.ErrorResponse> {
