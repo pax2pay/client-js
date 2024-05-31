0249e5d3e4f1eb9ca46f6d5a2ac0c2800419d93a
Magnus Gustafsson <magnus.gustafsson@pax2pay.com>
2024-05-31 13:30:52 +0200

    ui#1897 - Add merchantRestriction to CardResponseV2

--- model/CardResponseV2.ts
+++ model/CardResponseV2.ts
@@ -1,4 +1,5 @@
 import { Currency, Date } from "isoly"
+import { isly } from "isly"
 import { AccountState } from "./AccountState"
 import { BookingInfoResponse } from "./BookingInfoResponse"
 import { CardDeliveryResponse } from "./CardDeliveryResponse"
@@ -6,6 +7,7 @@ import { CardScheduleResponseItem } from "./CardScheduleResponseItem"
 import { CardTypeSpecification } from "./CardTypeSpecification"
 import { CardUsage } from "./CardUsage"
 import { FundingAccountSummaryResponse } from "./FundingAccountSummaryResponse"
+import { MerchantResponse } from "./MerchantResponse"
 import { ProviderCode } from "./ProviderCode"
 import { YearMonth } from "./YearMonth"
 
@@ -33,37 +35,35 @@ export interface CardResponseV2 {
 	bookingInfo?: BookingInfoResponse
 	delivery?: CardDeliveryResponse
 	batchId?: string
+	merchantRestriction?: MerchantResponse
 }
 
 export namespace CardResponseV2 {
-	export function is(value: CardResponseV2 | any): value is CardResponseV2 {
-		return (
-			typeof value == "object" &&
-			(typeof value.cardType == "string" || CardTypeSpecification.is(value.cardType)) &&
-			typeof value.cardNumber == "string" &&
-			(value.cvv == undefined || typeof value.cvv == "string") &&
-			YearMonth.is(value.expiryDate) &&
-			(value.nameOnCard == undefined || typeof value.nameOnCard == "string") &&
-			typeof value.fundingBalance == "number" &&
-			typeof value.remainingBalance == "number" &&
-			typeof value.balance == "number" &&
-			Date.is(value.issueDate) &&
-			ProviderCode.is(value.providerCode) &&
-			typeof value.providerCardId == "string" &&
-			CardUsage.is(value.usage) &&
-			FundingAccountSummaryResponse.is(value.fundingAccount) &&
-			(value.schedule == undefined ||
-				(Array.isArray(value.schedule) &&
-					value.schedule.every((a: any) => {
-						return CardScheduleResponseItem.is(a)
-					}))) &&
-			typeof value.createdBy == "string" &&
-			AccountState.is(value.state) &&
-			(value.delivery == undefined || CardDeliveryResponse.is(value.delivery)) &&
-			(value.batchId == undefined || typeof value.batchId == "string") &&
-			(value.bookingInfo == undefined || BookingInfoResponse.is(value.bookingInfo)) &&
-			(value.longTermTokenExpiry == undefined || Date.is(value.longTermTokenExpiry)) &&
-			(value.activationDate == undefined || Date.is(value.activationDate))
-		)
-	}
+	export const type = isly.object<CardResponseV2>({
+		cardType: isly.union(CardTypeSpecification.type, isly.string()),
+		cardNumber: isly.string(),
+		cvv: isly.string().optional(),
+		expiryDate: isly.fromIs("YearMonth", YearMonth.is),
+		nameOnCard: isly.string().optional(),
+		fundingBalance: isly.number(),
+		remainingBalance: isly.number(),
+		fundingDate: isly.string(),
+		balance: isly.number(),
+		currency: isly.fromIs("Currency", Currency.is),
+		issueDate: isly.string(),
+		providerCode: isly.fromIs("ProviderCode", ProviderCode.is),
+		providerCardId: isly.string(),
+		usage: isly.fromIs("CardUsage", CardUsage.is),
+		fundingAccount: isly.fromIs("FundingAccountSummaryResponse", FundingAccountSummaryResponse.is),
+		createdBy: isly.fromIs("Date", Date.is),
+		state: isly.fromIs("AccountState", AccountState.is),
+		longTermTokenExpiry: isly.fromIs("Date", Date.is).optional(),
+		activationDate: isly.fromIs("Date", Date.is).optional(),
+		schedule: isly.array(isly.fromIs("CardScheduleResponseItem", CardScheduleResponseItem.is)).optional(),
+		bookingInfo: isly.fromIs("BookingInfoResponse", BookingInfoResponse.is).optional(),
+		delivery: isly.fromIs("CardDeliveryResponse", CardDeliveryResponse.is).optional(),
+		batchId: isly.string().optional(),
+		merchantRestriction: isly.fromIs("MerchantResponse", MerchantResponse.is).optional(),
+	})
+	export const is = type.is
 }
