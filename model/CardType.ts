339e35ff21a35cbca34081c2ada9a7e0aff03f0d
qijieye <86956109+qijieye@users.noreply.github.com>
2024-07-19 15:18:21 +0200

    update CardType and CardSearchRequest (#233)

--- model/CardType.ts
+++ model/CardType.ts
@@ -1,21 +1,21 @@
 import { Currency } from "isoly"
+import { isly } from "isly"
 import { ProviderCode } from "./ProviderCode"
 
 export interface CardType {
 	providerCode: ProviderCode
 	cardType: string
-	representAs: string
 	cardName?: string
+	representAs?: string
 	currencies?: Currency[]
 }
 export namespace CardType {
-	export function is(value: CardType | any): value is CardType {
-		return (
-			typeof value == "object" &&
-			ProviderCode.is(value.providerCode) &&
-			typeof value.cardType == "string" &&
-			typeof value.representAs == "string" &&
-			(typeof value.cardName == "string" || value.cardName == undefined)
-		)
-	}
+	export const type = isly.object<CardType>({
+		providerCode: isly.fromIs("ProviderCode", ProviderCode.is),
+		cardType: isly.string(),
+		cardName: isly.string().optional(),
+		representAs: isly.string().optional(),
+		currencies: isly.array(isly.fromIs("Currency", Currency.is)).optional(),
+	})
+	export const is = type.is
 }
