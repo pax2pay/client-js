d3d2ea2cd74283e40a2be75c926a2ff406183295
Qijie Ye <qijie.ye@pax2pay.com>
2024-09-16 15:18:40 +0200

    update SecurityConfig

--- model/SecurityConfig.ts
+++ model/SecurityConfig.ts
@@ -1,8 +1,14 @@
+import { isly } from "isly"
+
 export interface SecurityConfig {
 	enforce2fa?: boolean
+	passwordExpirationDays?: number
+	passwordWith2faExpirationDays?: number
 }
 export namespace SecurityConfig {
-	export function is(value: SecurityConfig | any): value is SecurityConfig {
-		return typeof value == "object" && (typeof value.enforce2fa == "boolean" || value.enforce2fa == undefined)
-	}
+	export const type = isly.object<SecurityConfig>({
+		enforce2fa: isly.boolean().optional(),
+		passwordExpirationDays: isly.number().optional(),
+		passwordWith2faExpirationDays: isly.number().optional(),
+	})
 }
