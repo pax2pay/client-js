078260c0a7acffdcc24800207e162dae2727f058
Magnus Gustafsson <magnus.gustafsson@pax2pay.com>
2024-09-16 18:24:30 +0200

    Add PaymentSearch.providerCode

--- model/PaymentSearch.ts
+++ model/PaymentSearch.ts
@@ -1,15 +1,18 @@
 import { isly } from "isly"
+import { ProviderCode } from "./ProviderCode"
 
 export interface PaymentSearch {
 	paymentId?: string[]
 	cardId?: string[]
 	merchantId?: string[]
+	providerCode?: ProviderCode[]
 }
 export namespace PaymentSearch {
 	export const type = isly.object<PaymentSearch>({
-		paymentId: isly.array(isly.string()).optional(),
-		cardId: isly.array(isly.string()).optional(),
-		merchantId: isly.array(isly.string()).optional(),
+		paymentId: isly.string().array().optional(),
+		cardId: isly.string().array().optional(),
+		merchantId: isly.string().array().optional(),
+		providerCode: ProviderCode.type.array().optional(),
 	})
 	export const is = type.is
 }
