97b6fe15ef3219d3e3bc33834686a43a877698e1
Qijie Ye <qijie.ye@pax2pay.com>
2024-09-23 11:57:25 +0200

    ui#2225 - No card / payment id when downloading reciept from card batch create

---
+++ model/PaymentResponse.spec.ts
@@ -0,0 +1,64 @@
+import { PaymentResponse } from "./PaymentResponse"
+
+describe("PaymentResponse", () => {
+	it("is", () => {
+		expect(PaymentResponse.is({ status: "INACTIVE", code: "somecode" })).toBeFalsy()
+	})
+	it("is", () => {
+		const res = {
+			id: "Y000000DQ",
+			account: "A00003O",
+			amount: 1,
+			remaining: 1,
+			total: 301,
+			schedule: [
+				{
+					amount: 200,
+					date: "2024-12-10",
+				},
+				{
+					amount: 100,
+					date: "2024-12-16",
+				},
+			],
+			currency: "GBP",
+			state: "active",
+			method: "card",
+			meta: {
+				type: "FIVE_FIELDS",
+				trackingId: "xxxxxxxxxxxxxxxxxxxxxxxxx",
+				agentBookingReference: "test",
+				departureDate: "2024-08-29",
+				supplierBookingReference: "test",
+				leadPassengerName: "test",
+				supplierCode: "test",
+			},
+			createdBy: "qijiey",
+			createdOn: "2024-09-23T09:29:25.506830301",
+			delivery: {
+				type: "email",
+				to: "test@paxport.net",
+				message: "test message",
+				lapses: "2024-12-15",
+				sent: "2024-09-23T09:29:25.306692",
+				state: "pending",
+			},
+			card: {
+				id: "C0000000000000",
+				providerCode: "modulr",
+				providerCardId: "V210017WUW",
+				cardType: "P2P-140-Visa-Prepaid",
+				pan: "0000000000000000",
+				state: "active",
+				expires: "2024-12",
+				usage: "single",
+				cvv: "057",
+				cardHolderName: "test test",
+				issued: "2024-09-23",
+				remaining: 1,
+				maxAmount: 301,
+			},
+		}
+		expect(PaymentResponse.is(res)).toBeTruthy()
+	})
+})
