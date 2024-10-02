34978f7f6fb2338fff6ed404be81277f256f1a6e
Qijie Ye <qijie.ye@pax2pay.com>
2024-10-02 16:51:19 +0200

    add providerTransferId in TransferResponseV3

--- model/TransferResponseV3.ts
+++ model/TransferResponseV3.ts
@@ -3,11 +3,13 @@ import { isly } from "isly"
 export interface TransferResponseV3 {
 	beneficiary?: string
 	reference: string
+	providerTransferId: string
 }
 export namespace TransferResponseV3 {
 	export const type = isly.object<TransferResponseV3>({
 		beneficiary: isly.string().optional(),
 		reference: isly.string(),
+		providerTransferId: isly.string(),
 	})
 	export const is = type.is
 }
