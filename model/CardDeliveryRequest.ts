c2b18b970f4f8071824c3b2ddf4989ceedeb76af
Qijie Ye <qijie.ye@pax2pay.com>
2024-10-03 11:31:00 +0200

    review

--- model/CardDeliveryRequest.ts
+++ model/CardDeliveryRequest.ts
@@ -1,16 +1,19 @@
-import { Date } from "isoly"
+import * as isoly from "isoly"
 import { isly } from "isly"
 
 export interface CardDeliveryRequest {
 	to: string
 	message: string
-	linkExpiry: Date
+	linkExpiry: isoly.Date
 }
 export namespace CardDeliveryRequest {
 	export const type = isly.object<CardDeliveryRequest>({
-		to: isly.string(),
+		to: isly.string(/\S+@\S+\.\S+/),
 		message: isly.string(),
-		linkExpiry: isly.fromIs("Date", Date.is),
+		linkExpiry: isly.fromIs("Date", isExpiry),
 	})
+	function isExpiry(value: any): value is isoly.Date {
+		return isoly.Date.is(value) && value > isoly.Date.now()
+	}
 	export const is = type.is
 }
