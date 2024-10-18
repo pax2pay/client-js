0c0d2cfd696f026f5055b8bd7bd0689457fa1284
qijieye <86956109+qijieye@users.noreply.github.com>
2024-10-18 13:20:39 +0200

    using paginated for search transfers (#253)
    * using paginated but remove count
    
    * review

--- Client/Transfers/index.ts
+++ Client/Transfers/index.ts
@@ -2,6 +2,7 @@ import * as model from "../../model"
 import { ProviderCode } from "../../model/ProviderCode"
 import { Connection } from "../Connection"
 import { List } from "../List"
+import { Paginated } from "../Paginated"
 
 export class Transfers extends List<model.TransferResponse> {
 	protected folder = "transfers"
@@ -29,15 +30,25 @@ export class Transfers extends List<model.TransferResponse> {
 			otp ? { "x-otp": otp } : {}
 		)
 	}
-	async searchV2(request: model.TransferSearch, page?: number, size?: number, sort?: string) {
-		return await this.connection.post<{ list: model.TransferResponseV2[]; totalCount: number }>(
-			`v2/${this.folder}/searches`,
+	async searchV2(
+		request: model.TransferSearch,
+		previous?: Paginated<model.TransferResponseV2>,
+		page?: number,
+		size?: number,
+		sort?: string
+	) {
+		return await this.getNextPaginated(
+			previous,
+			(page, size, sort, request) =>
+				this.connection.post<{ list: model.TransferResponseV2[]; totalCount: number } | model.TransferResponseV2[]>(
+					`v2/${this.folder}/searches`,
+					request,
+					{ page, size, sort }
+				),
 			request,
-			{
-				page,
-				size,
-				sort,
-			}
+			page,
+			size,
+			sort
 		)
 	}
 	async approveV2(providerCode: ProviderCode, transferId: string, otp?: string) {
