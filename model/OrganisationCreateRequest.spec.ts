b13cfd0c4ea81d4b7ebfb43575befbe28db3dadf
Qijie Ye <qijie.ye@pax2pay.com>
2024-10-28 17:06:48 +0100

    use v2

--- model/OrganisationCreateRequest.spec.ts
+++ model/OrganisationCreateRequest.spec.ts
@@ -2,9 +2,9 @@ import { OrganisationCreateRequest } from "./OrganisationCreateRequest"
 
 describe("OrganisationCreateRequest", () => {
 	it("is", () => {
-		expect(OrganisationCreateRequest.is({ status: "INACTIVE", code: "somecode" })).toBeFalsy()
+		expect(OrganisationCreateRequest.is({ status: "inactive", code: "somecode" })).toBeFalsy()
 		expect(OrganisationCreateRequest.is({ name: "somename", code: "somecode" })).toBeFalsy()
-		expect(OrganisationCreateRequest.is({ name: "somename", status: "INACTIVE" })).toBeFalsy()
-		expect(OrganisationCreateRequest.is({ name: "somename", status: "INACTIVE", code: "somecode" })).toBeTruthy()
+		expect(OrganisationCreateRequest.is({ name: "somename", status: "inactive" })).toBeFalsy()
+		expect(OrganisationCreateRequest.is({ name: "somename", status: "inactive", code: "somecode" })).toBeTruthy()
 	})
 })
