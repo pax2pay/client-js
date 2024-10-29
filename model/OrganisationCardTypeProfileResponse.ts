af3779ee734d2cbf5883572e87f824d881fef86a
Qijie Ye <qijie.ye@pax2pay.com>
2024-10-29 17:04:03 +0100

    add type check

--- model/OrganisationCardTypeProfileResponse.ts
+++ model/OrganisationCardTypeProfileResponse.ts
@@ -1,6 +1,14 @@
+import { isly } from "isly"
 import { CardTypeProfileResponse } from "./CardTypeProfileResponse"
 
 export interface OrganisationCardTypeProfileResponse {
 	organisationCode: string
 	cardTypeProfiles: CardTypeProfileResponse[]
+}
+export namespace OrganisationCardTypeProfileResponse {
+	export const type = isly.object<OrganisationCardTypeProfileResponse>({
+		organisationCode: isly.string(),
+		cardTypeProfiles: isly.fromIs("CardTypeProfileResponse", CardTypeProfileResponse.is).array(),
+	})
+	export const is = type.is
 }
