46b0c9681bb9fb3ef7ceb982bf60835dbe773516
Qijie Ye <qijie.ye@pax2pay.com>
2024-10-31 10:58:55 +0100

    Keep OrganisationStatus and add method toLowerCase

--- model/OrganisationSearchResponse.ts
+++ model/OrganisationSearchResponse.ts
@@ -3,10 +3,13 @@ import { CardTypeInformation } from "./CardTypeInformation"
 import { InternalOrganisationConfig } from "./InternalOrganisationConfig"
 import { OrganisationConfig } from "./OrganisationConfig"
 import { OrganisationRealm } from "./OrganisationRealm"
-import { OrganisationResponse } from "./OrganisationResponse"
+import { OrganisationStatusV2 } from "./OrganisationStatusV2"
 import { ProviderCode } from "./ProviderCode"
 
-export interface OrganisationSearchResponse extends OrganisationResponse {
+export interface OrganisationSearchResponse {
+	code: string
+	name: string
+	status: OrganisationStatusV2
 	realm?: OrganisationRealm | OrganisationRealm[]
 	credentials?: Partial<Record<ProviderCode, Record<string, any>>>
 	organisationConfig?: OrganisationConfig
@@ -15,14 +18,15 @@ export interface OrganisationSearchResponse extends OrganisationResponse {
 }
 
 export namespace OrganisationSearchResponse {
-	export const type = OrganisationResponse.type.extend<OrganisationSearchResponse>({
+	export const type = isly.object<OrganisationSearchResponse>({
+		code: isly.string(),
+		name: isly.string(),
+		status: OrganisationStatusV2.type,
 		realm: isly.union(OrganisationRealm.type, isly.array(OrganisationRealm.type)).optional(),
-		credentials: isly.record(isly.fromIs("ProviderCode", ProviderCode.is), isly.any()).optional(),
+		credentials: isly.record(ProviderCode.type, isly.any()).optional(),
 		organisationConfig: isly.fromIs("OrganisationConfig", OrganisationConfig.is).optional(),
 		internalOrganisationConfig: isly.fromIs("InternalOrganisationConfig", InternalOrganisationConfig.is).optional(),
-		cardTypes: isly
-			.record(isly.fromIs("ProviderCode", ProviderCode.is), isly.fromIs("CardTypeInformation", CardTypeInformation.is))
-			.optional(),
+		cardTypes: isly.record(ProviderCode.type, isly.fromIs("CardTypeInformation", CardTypeInformation.is)).optional(),
 	})
 	export const is = type.is
 }
--- model/OrganisationStatus.ts
+++ model/OrganisationStatus.ts
@@ -1,9 +1,13 @@
 import { isly } from "isly"
+import { OrganisationStatusV2 } from "./OrganisationStatusV2"
 
 export type OrganisationStatus = typeof OrganisationStatus.values[number]
 
 export namespace OrganisationStatus {
-	export const values = ["active", "inactive", "deleted"] as const
+	export const values = ["ACTIVE", "INACTIVE", "DELETED"] as const
 	export const type = isly.string<OrganisationStatus>(values)
 	export const is = type.is
+	export function toLowerCase(value: OrganisationStatus): OrganisationStatusV2 {
+		return value.toLowerCase() as OrganisationStatusV2
+	}
 }
