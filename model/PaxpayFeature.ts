1863af03468b743c8a38ac06ede4adf107f26453
Magnus Gustafsson <magnus.gustafsson@pax2pay.com>
2024-11-11 17:01:37 +0100

    ui#2391 - Add HIDE_EMPTY_STATEMENTS paxpayFeature (#258)
    * ui#2391 - Add HIDE_EMPTY_STATEMENTS paxpayFeature
    
    * Add back BATCH_PAYMENTS and FIND_AND_AMEND
    
    * Removed again

--- model/PaxpayFeature.ts
+++ model/PaxpayFeature.ts
@@ -1,26 +1,13 @@
-const paxpayFeatures = [
-	"ALLOW_CARD_CREATION_WITH_NEGATIVE_BALANCE",
-	"FUZZY_SEARCH_COLLECTION_TRAVERSAL",
-	"BANK_TRANSFERS",
-	"BATCH_PAYMENTS",
-	"FIND_AND_AMEND",
-	"BETA_PORTAL_FEATURES",
-	"LEGACY_CARD_CREATE",
-	"LEGACY_CARD_DETAILS",
-	"LEGACY_CARD_SINGLE",
-	"LEGACY_ACCOUNT_TABLE",
-	"LEGACY_USER_TABLE",
-	"LEGACY_CARD_TABLE",
-	"SWITCHER_PORTAL_ONLY",
-	"SWITCHER_PORTAL",
-	"SWITCHER_POMS_ONLY",
-	"SWITCHER_POMS",
-] as const
-
-export type PaxpayFeature = typeof paxpayFeatures[number]
+export type PaxpayFeature = typeof PaxpayFeature.values[number]
 
 export namespace PaxpayFeature {
+	export const values = [
+		"ALLOW_CARD_CREATION_WITH_NEGATIVE_BALANCE",
+		"FUZZY_SEARCH_COLLECTION_TRAVERSAL",
+		"BANK_TRANSFERS",
+		"HIDE_EMPTY_STATEMENTS",
+	] as const
 	export function is(value: unknown): value is PaxpayFeature {
-		return typeof value == "string" && paxpayFeatures.includes(value as PaxpayFeature)
+		return typeof value == "string" && values.includes(value as PaxpayFeature)
 	}
 }
