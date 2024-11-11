9d2f625e64d769613f1e7831f6a75008e072e5ac
Magnus Gustafsson <magnus.gustafsson@pax2pay.com>
2024-11-11 16:41:38 +0100

    Removed again

--- model/PaxpayFeature.ts
+++ model/PaxpayFeature.ts
@@ -6,8 +6,6 @@ export namespace PaxpayFeature {
 		"FUZZY_SEARCH_COLLECTION_TRAVERSAL",
 		"BANK_TRANSFERS",
 		"HIDE_EMPTY_STATEMENTS",
-		"BATCH_PAYMENTS",
-		"FIND_AND_AMEND",
 	] as const
 	export function is(value: unknown): value is PaxpayFeature {
 		return typeof value == "string" && values.includes(value as PaxpayFeature)
