2238ad164d82a3cbe0986783227d5bb3c0588c4e
Magnus Gustafsson <magnus.gustafsson@pax2pay.com>
2024-11-12 11:35:42 +0100

    ui#2391 - Add SHOW_EMPTY_STATEMENTS (#259)

--- model/PaxpayFeature.ts
+++ model/PaxpayFeature.ts
@@ -5,7 +5,7 @@ export namespace PaxpayFeature {
 		"ALLOW_CARD_CREATION_WITH_NEGATIVE_BALANCE",
 		"FUZZY_SEARCH_COLLECTION_TRAVERSAL",
 		"BANK_TRANSFERS",
-		"HIDE_EMPTY_STATEMENTS",
+		"SHOW_EMPTY_STATEMENTS",
 	] as const
 	export function is(value: unknown): value is PaxpayFeature {
 		return typeof value == "string" && values.includes(value as PaxpayFeature)
