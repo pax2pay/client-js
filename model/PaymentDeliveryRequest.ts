bac449d344c939cf889483aab60eb570f51fb647
Magnus Gustafsson <magnus.gustafsson@pax2pay.com>
2024-11-18 13:20:59 +0100

    ui#2402 - Set PaymentDeliveryRequest.lapses to optional (#261)

--- model/PaymentDeliveryRequest.ts
+++ model/PaymentDeliveryRequest.ts
@@ -5,14 +5,14 @@ export interface PaymentDeliveryRequest {
 	type?: string
 	to: string
 	message: string
-	lapses: Date
+	lapses?: Date
 }
 export namespace PaymentDeliveryRequest {
 	export const type = isly.object<PaymentDeliveryRequest>({
 		type: isly.string().optional(),
 		to: isly.string(),
 		message: isly.string(),
-		lapses: isly.fromIs("Date", Date.is),
+		lapses: isly.fromIs("Date", Date.is).optional(),
 	})
 	export const is = type.is
 }
