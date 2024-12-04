3ef5084e3ac0e0590dd3878f220cdbca6af52d01
qijieye <qijie.ye@pax2pay.com>
2024-12-04 13:50:08 +0100

    update MerchantRequest

--- model/BeneficiaryRequest.ts
+++ model/BeneficiaryRequest.ts
@@ -1,9 +1,16 @@
+import { isly } from "isly"
 import { TransferDestinationInfo } from "./TransferDestinationInfo"
 
 export interface BeneficiaryRequest {
 	transferDestination: TransferDestinationInfo
 	defaultReference?: string
-	status: "ACTIVE" | "DELETED" | "OUTDATED"
 	name?: string
-	fullName: string
+}
+export namespace BeneficiaryRequest {
+	export const type = isly.object<BeneficiaryRequest>({
+		transferDestination: isly.fromIs("TransferDestinationInfo", TransferDestinationInfo.is),
+		defaultReference: isly.string().optional(),
+		name: isly.string().optional(),
+	})
+	export const is = type.is
 }
--- model/MerchantRequest.ts
+++ model/MerchantRequest.ts
@@ -1,19 +1,27 @@
+import { Currency } from "isoly"
 import { isly } from "isly"
+import { BeneficiaryRequest } from "./BeneficiaryRequest"
 import { MerchantType } from "./MerchantType"
 
 export interface MerchantRequest {
 	id?: string
-	name?: string
+	name: string
 	mcc?: string
-	type?: MerchantType
+	type: MerchantType
+	logoUrl?: string
+	beneficiaries?: Partial<Record<Currency, BeneficiaryRequest>>
 }
 
 export namespace MerchantRequest {
 	export const type = isly.object<MerchantRequest>({
 		id: isly.string().optional(),
-		name: isly.string().optional(),
+		name: isly.string(),
 		mcc: isly.string().optional(),
-		type: MerchantType.type.optional(),
+		type: MerchantType.type,
+		logoUrl: isly.string().optional(),
+		beneficiaries: isly
+			.record(isly.fromIs("Currency", Currency.is), isly.fromIs("BeneficiaryRequest", BeneficiaryRequest.is))
+			.optional(),
 	})
 	export const is = type.is
 }
