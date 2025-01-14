baf7ff1404e2d83d6c39cc1c52a8472afc636821
Magnus Gustafsson <magnus.gustafsson@pax2pay.com>
2025-01-14 14:18:55 +0100

    add CardState.toLowerCaseName

--- model/CardState.ts
+++ model/CardState.ts
@@ -4,6 +4,20 @@ export type CardState = typeof CardState.values[number]
 
 export namespace CardState {
 	export const values = ["ACTIVE", "INACTIVE", "DELETED", "EXPIRED", "PENDING", "APPROVED", "DECLINED"] as const
+
 	export const type = isly.string<CardState>(values)
 	export const is = type.is
+	export function toLowerCaseName(value: CardState) {
+		return (
+			{
+				ACTIVE: "active",
+				INACTIVE: "frozen",
+				DELETED: "closed",
+				EXPIRED: "expired",
+				PENDING: "pending",
+				APPROVED: "approved",
+				DECLINED: "declined",
+			} as const
+		)[value]
+	}
 }
