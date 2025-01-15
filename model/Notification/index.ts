cda650264fcad1e3529c5760ce2e9947e53aeddd
Magnus Gustafsson <magnus.gustafsson@pax2pay.com>
2025-01-15 15:22:09 +0100

    Remove Search

--- model/Notification/index.ts
+++ model/Notification/index.ts
@@ -1,7 +1,6 @@
 import { Format as NotificationFormat } from "./Format"
 import { InsertRequest as NotificationInsertRequest } from "./InsertRequest"
 import { Response as NotificationResponse } from "./Response"
-import { Search as NotificationSearch } from "./Search"
 import { Tag as NotificationTag } from "./Tag"
 import { Type as NotificationType } from "./Type"
 
@@ -9,7 +8,6 @@ export namespace Notification {
 	export type Format = NotificationFormat
 	export type InsertRequest = NotificationInsertRequest
 	export type Response = NotificationResponse
-	export type Search = NotificationSearch
 	export type Tag = NotificationTag
 	export type Type = NotificationType
 }
