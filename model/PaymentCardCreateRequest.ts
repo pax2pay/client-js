4d71b16ffaec1324f044fbb7614b8b5bf88506e2
qijieye <qijie.ye@pax2pay.com>
2025-01-31 15:17:27 +0100

    update PaymentCardCreateRequest

--- model/PaymentCardCreateRequest.ts
+++ model/PaymentCardCreateRequest.ts
@@ -1,16 +1,20 @@
+import * as isoly from "isoly"
 import { isly } from "isly"
+import { CardTypeSpecification } from "./CardTypeSpecification"
 import { CardUsage } from "./CardUsage"
 
 export interface PaymentCardCreateRequest {
-	cardType: string
+	cardType: CardTypeSpecification
 	expires?: string
 	usage?: CardUsage
+	closeDate?: isoly.Date
 }
 export namespace PaymentCardCreateRequest {
 	export const type = isly.object<PaymentCardCreateRequest>({
-		cardType: isly.string(),
+		cardType: isly.fromIs("CardTypeSpecification", CardTypeSpecification.is),
 		expires: isly.string().optional(),
 		usage: CardUsage.type.optional(),
+		closeDate: isly.fromIs("Date", isoly.Date.is).optional(),
 	})
 	export const is = type.is
 }
