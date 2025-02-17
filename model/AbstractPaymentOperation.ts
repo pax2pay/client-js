994be021cca2335dd672c3791526d1d3140a16c8
qijieye <qijie.ye@pax2pay.com>
2025-02-17 14:55:29 +0100

    update

--- model/AbstractPaymentOperation.ts
+++ model/AbstractPaymentOperation.ts
@@ -4,11 +4,13 @@ import { isly } from "isly"
 export interface AbstractPaymentOperation {
 	type: string
 	timestamp: DateTime
+	description?: string
 }
 export namespace AbstractPaymentOperation {
 	export const type = isly.object<AbstractPaymentOperation>({
 		type: isly.string(),
 		timestamp: isly.fromIs("DateTime", DateTime.is),
+		description: isly.string().optional(),
 	})
 	export const is = type.is
 }
