8d5bc76a1d7c31faaaa21be0b81bca52b35beb02
qijieye <86956109+qijieye@users.noreply.github.com>
2025-02-26 14:15:25 +0100

    Add payment tokenised api and update TransferResponseV3 (#289)
    * add create tokenised payment
    
    * update TransferResponseV3
    
    * update

--- model/TransferResponseV3.ts
+++ model/TransferResponseV3.ts
@@ -1,15 +1,21 @@
 import { isly } from "isly"
+import { ProviderCode } from "./ProviderCode"
+import { TransferDestinationInfo } from "./TransferDestinationInfo"
 
 export interface TransferResponseV3 {
 	beneficiary?: string
 	reference: string
 	providerTransferId: string
+	providerCode: ProviderCode
+	destination: TransferDestinationInfo
 }
 export namespace TransferResponseV3 {
 	export const type = isly.object<TransferResponseV3>({
 		beneficiary: isly.string().optional(),
 		reference: isly.string(),
 		providerTransferId: isly.string(),
+		providerCode: ProviderCode.type,
+		destination: isly.fromIs("TransferDestinationInfo", TransferDestinationInfo.is),
 	})
 	export const is = type.is
 }
