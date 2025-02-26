f115f88186e91218d9042aaece58a3baaec1f38f
qijieye <qijie.ye@pax2pay.com>
2025-02-26 13:47:40 +0100

    update TransferResponseV3

--- model/TransferResponseV3.ts
+++ model/TransferResponseV3.ts
@@ -1,15 +1,21 @@
 import { isly } from "isly"
+import { ProviderCode } from "./ProviderCode"
+import { TransferDestinationResponse } from "./TransferDestinationResponse"
 
 export interface TransferResponseV3 {
 	beneficiary?: string
 	reference: string
 	providerTransferId: string
+	providerCode: ProviderCode
+	destination: TransferDestinationResponse
 }
 export namespace TransferResponseV3 {
 	export const type = isly.object<TransferResponseV3>({
 		beneficiary: isly.string().optional(),
 		reference: isly.string(),
 		providerTransferId: isly.string(),
+		providerCode: ProviderCode.type,
+		destination: isly.fromIs("TransferDestinationResponse", TransferDestinationResponse.is),
 	})
 	export const is = type.is
 }
