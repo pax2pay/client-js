6a68a21db0058dee72ca8e0e27fb90583a5bdfd1
qijieye <qijie.ye@pax2pay.com>
2025-04-24 10:34:04 +0200

    revert

--- Client/Beneficiaries/index.ts
+++ Client/Beneficiaries/index.ts
@@ -36,9 +36,6 @@ export class Beneficiaries extends List<model.BeneficiaryResponse> {
 		)
 		return this.extractResponse(response)
 	}
-	async confirmPayee(request: model.ConfirmationOfPayeeRequest) {
-		return await this.connection.post<model.ConfirmationOfPayeeResponse>(`confirmation-of-payee`, request)
-	}
 	async create(request: model.BeneficiaryRequest) {
 		return await this.connection.post<model.BeneficiaryResponse>(`${this.folder}`, request)
 	}
