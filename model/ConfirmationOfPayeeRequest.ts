b8c4cec11025e906682b35c2d29e212616a9f7f9
qijieye <qijie.ye@pax2pay.com>
2025-04-28 16:18:30 +0200

    update

--- model/ConfirmationOfPayeeRequest.ts
+++ model/ConfirmationOfPayeeRequest.ts
@@ -4,12 +4,14 @@ export interface ConfirmationOfPayeeRequest {
 	accountNumber: string
 	sortCode: string
 	payeeName: string
+	sourceAccountId?: string
 }
 export namespace ConfirmationOfPayeeRequest {
 	export const type = isly.object<ConfirmationOfPayeeRequest>({
 		accountNumber: isly.string(),
 		sortCode: isly.string(),
 		payeeName: isly.string(),
+		sourceAccountId: isly.string().optional(),
 	})
 	export const is = type.is
 }
