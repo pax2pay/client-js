8a704e4898d102e017fe69c1a7758104fee99f74
qijieye <86956109+qijieye@users.noreply.github.com>
2025-05-21 14:25:59 +0200

    Add ability to assume desired org when fetch card types (#317)
    * update
    
    * minor fix

--- Client/Cards/index.ts
+++ Client/Cards/index.ts
@@ -105,9 +105,15 @@ export class Cards extends List<model.CardResponseV2 | model.CardResponse> {
 		)
 	}
 	//Possibly should be moved to its own class
-	async getCardTypesV2(providerCode?: model.ProviderCode): Promise<model.ErrorResponse | model.CardTypeResponse[]> {
+	async getCardTypesV2(
+		providerCode?: model.ProviderCode,
+		assumedOrg?: string
+	): Promise<model.ErrorResponse | model.CardTypeResponse[]> {
+		const header = assumedOrg ? { "x-assume": assumedOrg } : undefined
 		const response = await this.connection.get<{ list: model.CardTypeResponse[]; totalCount: number }>(
-			`v2/${this.folder}/types/${providerCode ?? ""}`
+			`v2/${this.folder}/types${providerCode ? `/${providerCode}` : ""}`,
+			undefined,
+			header
 		)
 		return this.extractResponse<model.CardTypeResponse>(response)
 	}
