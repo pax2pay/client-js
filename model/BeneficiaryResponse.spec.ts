6cdf070d2b64763f702cf3bd47c1ffd101b1c631
Magnus Gustafsson <magnus.gustafsson@pax2pay.com>
2025-06-03 14:33:38 +0200

    Add test

--- model/BeneficiaryResponse.spec.ts
+++ model/BeneficiaryResponse.spec.ts
@@ -13,6 +13,21 @@ describe("BeneficiaryResponse", () => {
 					currency: "EUR",
 				},
 				status: "ACTIVE",
+				name: "Jane Doe",
+				beneficiaryId: "B12345",
+				createdOn: "2023-10-01T00:00:00Z",
+			},
+			true,
+		],
+		[
+			{
+				transferDestination: {
+					iban: "DE89370400440532013000",
+					bic: "DEUTDEDBFRA",
+					type: "IBAN",
+					currency: "EUR",
+				},
+				status: "ACTIVE",
 				name: "John Doe",
 				beneficiaryId: "B12345",
 				createdOn: "2023-10-01T00:00:00Z",
