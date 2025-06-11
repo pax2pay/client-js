00cf442eefacb4c8f0808939247791ec6adabbce
Magnus Gustafsson <magnus.gustafsson@pax2pay.com>
2025-06-11 10:10:53 +0200

    Add DownloadReconciliationReportRequest.is

--- model/DownloadReconciliationReportRequest.ts
+++ model/DownloadReconciliationReportRequest.ts
@@ -1,4 +1,5 @@
 import * as isoly from "isoly"
+import { isly } from "isly"
 import { DownloadFileFormat } from "./DownloadFileFormat"
 import { ProviderCode } from "./ProviderCode"
 export interface DownloadReconciliationReportRequest {
@@ -7,8 +8,17 @@ export interface DownloadReconciliationReportRequest {
 	providerCode?: ProviderCode
 	providerAccountId?: string
 	version?: "LEGACY" | "V2"
-	dateRange?: {
-		start: isoly.Date
-		end: isoly.Date
-	}
+	dateRange?: isoly.DateRange
+}
+
+export namespace DownloadReconciliationReportRequest {
+	export const type = isly.object<DownloadReconciliationReportRequest>({
+		fileFormat: DownloadFileFormat.type,
+		locale: isly.string().optional(),
+		providerCode: ProviderCode.type.optional(),
+		providerAccountId: isly.string().optional(),
+		version: isly.string(["LEGACY", "V2"]).optional(),
+		dateRange: isly.fromIs("DateRange", isoly.DateRange.is).optional(),
+	})
+	export const is = type.is
 }
