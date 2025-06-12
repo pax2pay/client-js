c2c8a4983c06e46df5d6b7fcf22953e125c8b03e
Magnus Gustafsson <magnus.gustafsson@pax2pay.com>
2025-06-12 17:03:35 +0200

    Remove v1 transfer api calls

--- Client/Transfers/index.ts
+++ Client/Transfers/index.ts
@@ -12,16 +12,6 @@ export class Transfers extends List<model.TransferResponse> {
 	static create(connection: Connection): Transfers {
 		return new Transfers(connection)
 	}
-	async getAll(): Promise<model.ErrorResponse | model.TransferResponse[]> {
-		const response = await this.connection.get<{ list: model.TransferResponse[]; totalCount: number }>(this.folder)
-		return this.extractResponse(response)
-	}
-	async getTransfer(provider: model.ProviderCode, transferId: string) {
-		return await this.connection.get<model.TransferResponse>([this.folder, provider, transferId].join("/"))
-	}
-	async create(request: model.TransferRequest) {
-		return await this.connection.post<model.TransferResponse>(this.folder, request)
-	}
 	async createV2(request: model.TransferRequest, otp?: string) {
 		return await this.connection.post<model.TransferResponseV2>(
 			`v2/${this.folder}`,
