2fa68d0ad3800389feac91b51af6998857c09c8a
qijieye <qijie.ye@pax2pay.com>
2025-06-30 13:49:12 +0200

    update

--- Client/Payments/index.ts
+++ Client/Payments/index.ts
@@ -76,7 +76,7 @@ export class Payments extends List<model.PaymentResponse> {
 	async editMetadata(id: string, request: model.EditPaymentMetadataRequest) {
 		return await this.connection.put<model.PaymentResponse>(`${this.folder}/${id}/meta`, request)
 	}
-	async download(id: string) {
+	async downloadReceipt(id: string) {
 		return await this.connection.get<model.DownloadableResponse>(`${this.folder}/${id}/download`)
 	}
 }
