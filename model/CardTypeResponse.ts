f67196d8a47f501c397f0746eb6486760173cfc7
Magnus Gustafsson <magnus.gustafsson@pax2pay.com>
2025-07-03 10:50:22 +0200

    Remove CardType (#333)

--- model/CardTypeResponse.ts
+++ model/CardTypeResponse.ts
@@ -1,4 +1,5 @@
 import { Currency } from "isoly"
+import { isly } from "isly"
 import { CardTypeFlag } from "./CardTypeSpecificationFlag"
 import { FundingType } from "./FundingType"
 import { ProviderCode } from "./ProviderCode"
@@ -18,4 +19,21 @@ export interface CardTypeResponse {
 	preActive?: boolean
 	discontinued?: boolean
 	usages?: Usage[]
+}
+
+export namespace CardTypeResponse {
+	export const type = isly.object<CardTypeResponse>({
+		providerCode: isly.fromIs("ProviderCode", ProviderCode.is),
+		cardTypeId: isly.string(),
+		description: isly.string(),
+		originalDescription: isly.string().optional(),
+		scheme: Scheme.type,
+		funding: FundingType.type.optional(),
+		currencies: isly.array(isly.fromIs("Currency", Currency.is)).optional(),
+		flags: isly.array(CardTypeFlag.type).optional(),
+		bins: isly.array(isly.string()).optional(),
+		preActive: isly.boolean().optional(),
+		discontinued: isly.boolean().optional(),
+		usages: isly.array(Usage.type).optional(),
+	})
 }
