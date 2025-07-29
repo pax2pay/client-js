687b7d0de2afb6d04b651ab93cb38198358ab247
qijieye <qijie.ye@pax2pay.com>
2025-07-29 10:12:03 +0200

    update ConfirmationOfPayeeResponse

--- model/ConfirmationOfPayeeResponse.ts
+++ model/ConfirmationOfPayeeResponse.ts
@@ -5,12 +5,18 @@ export interface ConfirmationOfPayeeResponse {
 	status: ConfirmationOfPayeeResponseStatus
 	message?: string
 	nameSuggestion?: string
+	description?: string
+	acceptedStatus?: ConfirmationOfPayeeResponseStatus
+	acceptedByUser?: string
 }
 export namespace ConfirmationOfPayeeResponse {
 	export const type = isly.object<ConfirmationOfPayeeResponse>({
 		status: ConfirmationOfPayeeResponseStatus.type,
 		message: isly.string().optional(),
 		nameSuggestion: isly.string().optional(),
+		description: isly.string().optional(),
+		acceptedStatus: ConfirmationOfPayeeResponseStatus.type.optional(),
+		acceptedByUser: isly.string().optional(),
 	})
 	export const is = type.is
 }
