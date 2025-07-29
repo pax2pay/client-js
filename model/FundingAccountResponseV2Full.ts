1ff81726579ab0554c650fc8e693ced0bf5b0bb9
qijieye <qijie.ye@pax2pay.com>
2025-07-29 16:17:21 +0200

    update

--- model/FundingAccountResponseV2Full.ts
+++ model/FundingAccountResponseV2Full.ts
@@ -1,11 +1,9 @@
 import { DateTime } from "isoly"
 import { AccountIdentifierResponse } from "./AccountIdentifierResponse"
 import { FundingAccountResponseV2Basic } from "./FundingAccountResponseV2Basic"
-import { FundingLimitResponse } from "./FundingLimitResponse"
 
 export interface FundingAccountResponseV2Full extends FundingAccountResponseV2Basic {
 	actualBalance?: number
-	fundingLimit?: FundingLimitResponse
 	accountIdentifier?: AccountIdentifierResponse
 	totalCardAvailableBalance?: number
 	updatedOn: DateTime
@@ -21,7 +19,6 @@ export namespace FundingAccountResponseV2Full {
 			(value.actualBalance == undefined || typeof value.actualBalance == "number") &&
 			(value.realm == undefined || typeof value.realm == "string") &&
 			(value.legalEntityName == undefined || typeof value.legalEntityName == "string") &&
-			(value.fundingLimit == undefined || FundingLimitResponse.is(value.fundingLimit)) &&
 			(value.accountIdentifier == undefined || AccountIdentifierResponse.is(value.accountIdentifier)) &&
 			(value.totalCardAvailableBalance == undefined || typeof value.totalCardAvailableBalance == "number") &&
 			DateTime.is(value.updatedOn) &&
