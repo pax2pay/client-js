f478c1a100fb5bc76c860a9d24c56b57519c993b
qijieye <86956109+qijieye@users.noreply.github.com>
2025-08-22 11:44:38 +0200

    update cop response (#342)
    * update cop response
    
    * remove accepted

--- model/ConfirmationOfPayeeResponse.ts
+++ model/ConfirmationOfPayeeResponse.ts
@@ -4,27 +4,23 @@ import { ConfirmationOfPayeeResponseStatus } from "./ConfirmationOfPayeeResponse
 
 export interface ConfirmationOfPayeeResponse {
 	status: ConfirmationOfPayeeResponseStatus
-	message?: string
 	nameSuggestion?: string
 	description?: string
-	acceptedStatus?: ConfirmationOfPayeeResponseStatus
+	accepted?: boolean
 	acceptedByUser?: string
 	accountType?: ConfirmationOfPayeeAccountType
 	secondaryAccountId?: string
-	sourceAccountId?: string
 	acceptId?: string
 }
 export namespace ConfirmationOfPayeeResponse {
 	export const type = isly.object<ConfirmationOfPayeeResponse>({
 		status: ConfirmationOfPayeeResponseStatus.type,
-		message: isly.string().optional(),
 		nameSuggestion: isly.string().optional(),
 		description: isly.string().optional(),
-		acceptedStatus: ConfirmationOfPayeeResponseStatus.type.optional(),
+		accepted: isly.boolean().optional(),
 		acceptedByUser: isly.string().optional(),
 		accountType: ConfirmationOfPayeeAccountType.type.optional(),
 		secondaryAccountId: isly.string().optional(),
-		sourceAccountId: isly.string().optional(),
 		acceptId: isly.string().optional(),
 	})
 	export const is = type.is
