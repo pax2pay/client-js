e005646e6b6b31f3e9a2dc4ac3606f1392ba2486
qijieye <86956109+qijieye@users.noreply.github.com>
2025-09-03 15:21:29 +0200

    add new state archive in account state (#345)

--- model/AccountState.ts
+++ model/AccountState.ts
@@ -1,9 +1,37 @@
-const accountState = ["ACTIVE", "INACTIVE", "DELETED", "EXPIRED", "PENDING", "APPROVED", "DECLINED"] as const
+import { isly } from "isly"
 
-export type AccountState = typeof accountState[number]
+export type AccountState = typeof AccountState.values[number]
 
 export namespace AccountState {
-	export function is(value: unknown): value is AccountState {
-		return typeof value == "string" && accountState.includes(value as AccountState)
+	export const values = [
+		"ACTIVE",
+		"INACTIVE",
+		"DELETED",
+		"EXPIRED",
+		"PENDING",
+		"APPROVED",
+		"DECLINED",
+		"ARCHIVED",
+	] as const
+	export const type = isly.string<AccountState>(values)
+	export const is = type.is
+	export function toDisplay(value: AccountState): string {
+		if (value == "ACTIVE")
+			return "active"
+		if (value == "INACTIVE")
+			return "frozen"
+		if (value == "DELETED")
+			return "closed"
+		if (value == "EXPIRED")
+			return "expired"
+		if (value == "PENDING")
+			return "pending"
+		if (value == "APPROVED")
+			return "approved"
+		if (value == "DECLINED")
+			return "declined"
+		if (value == "ARCHIVED")
+			return "archived"
+		return value
 	}
 }
