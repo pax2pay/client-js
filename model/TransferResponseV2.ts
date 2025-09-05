0bfcf7aaa7830009fa2dd4ff44cffc2fe726416c
qijieye <qijie.ye@pax2pay.com>
2025-09-05 10:31:18 +0200

    update TransferResponseV2

--- model/TransferResponseV2.ts
+++ model/TransferResponseV2.ts
@@ -1,4 +1,5 @@
 import { Currency, Date, DateTime } from "isoly"
+import { isly } from "isly"
 import { ExternalSource } from "./ExternalSource"
 import { FundingAccountSummaryResponse } from "./FundingAccountSummaryResponse"
 import { MetadataResponse } from "./MetadataResponse"
@@ -20,7 +21,7 @@ export interface TransferResponseV2 {
 	source: FundingAccountSummaryResponse | ExternalSource
 	destination: TransferDestinationResponse
 	bookingInfo?: MetadataResponse
-	direction: TransferDirection
+	direction?: TransferDirection
 	createdBy: string
 	personallyApprovable?: boolean
 	approvedBy?: string
@@ -28,26 +29,27 @@ export interface TransferResponseV2 {
 }
 
 export namespace TransferResponseV2 {
-	export function is(value: TransferResponseV2 | any): value is TransferResponseV2 {
-		return (
-			typeof value == "object" &&
-			ProviderCode.is(value.providerCode) &&
-			typeof value.providerTransferId == "string" &&
-			typeof value.amount == "number" &&
-			Currency.is(value.currency) &&
-			TransferStatus.is(value.status) &&
-			(value.errorMessage == undefined || typeof value.errorMessage == "string") &&
-			Date.is(value.createdDate) &&
-			(value.paymentDate == undefined || Date.is(value.paymentDate)) &&
-			typeof value.reference == "string" &&
-			FundingAccountSummaryResponse.is(value.source) &&
-			TransferDestinationResponse.is(value.destination) &&
-			(value.bookingInfo == undefined || MetadataResponse.is(value.bookingInfo)) &&
-			TransferDirection.is(value.direction) &&
-			typeof value.createdBy == "string" &&
-			(value.personallyApprovable == undefined || typeof value.personallyApprovable == "boolean") &&
-			(value.approvedBy == undefined || typeof value.approvedBy == "string") &&
-			(value.approvedOn == undefined || DateTime.is(value.approvedOn))
-		)
-	}
+	export const type = isly.object<TransferResponseV2>({
+		providerCode: ProviderCode.type,
+		providerTransferId: isly.string(),
+		amount: isly.number(),
+		currency: isly.fromIs("Currency", Currency.is),
+		status: TransferStatus.type,
+		errorMessage: isly.string().optional(),
+		createdDate: isly.fromIs("Date", Date.is),
+		paymentDate: isly.fromIs("Date", Date.is).optional(),
+		reference: isly.string(),
+		source: isly.union(
+			isly.fromIs("FundingAccountSummaryResponse", FundingAccountSummaryResponse.is),
+			isly.fromIs("ExternalSource", ExternalSource.is)
+		),
+		destination: isly.fromIs("TransferDestinationResponse", TransferDestinationResponse.is),
+		bookingInfo: MetadataResponse.type.optional(),
+		direction: isly.fromIs("TransferDirection", TransferDirection.is).optional(),
+		createdBy: isly.string(),
+		personallyApprovable: isly.boolean().optional(),
+		approvedBy: isly.string().optional(),
+		approvedOn: isly.fromIs("DateTime", DateTime.is).optional(),
+	})
+	export const is = type.is
 }
