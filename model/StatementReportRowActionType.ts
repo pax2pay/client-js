da65eb7f06f0f28adaecff5a390e6f489965bd59
Magnus Gustafsson <magnus.gustafsson@pax2pay.com>
2025-09-08 15:51:52 +0200

    ui#3220 - Add actionTypes to StatementReportRequest (#348)
    * ui#3220 - Add actionTypes to StatementReportRequest
    
    * actionTypes is array

--- model/StatementReportRowActionType.ts
+++ model/StatementReportRowActionType.ts
@@ -1,23 +1,24 @@
-const statementReportRowActionType = [
-	"AUTHORISATION",
-	"SETTLEMENT",
-	"REVERSAL",
-	"REFUND",
-	"AUTHORISATION_FEE",
-	"SETTLEMENT_FEE",
-	"REVERSAL_FEE",
-	"REFUND_FEE",
-	"TRANSFER_IN",
-	"TRANSFER_OUT",
-	"USABLE_CARD",
-	"FUTURE_TRANSFER",
-	"SCHEDULED_CARD_AMENDMENT",
-] as const
-export type StatementReportRowActionType = typeof statementReportRowActionType[number]
+export type StatementReportRowActionType = typeof StatementReportRowActionType.values[number]
 
 export namespace StatementReportRowActionType {
+	export const values = [
+		"AUTHORISATION",
+		"SETTLEMENT",
+		"REVERSAL",
+		"REFUND",
+		"AUTHORISATION_FEE",
+		"SETTLEMENT_FEE",
+		"REVERSAL_FEE",
+		"REFUND_FEE",
+		"TRANSFER_IN",
+		"TRANSFER_OUT",
+		"USABLE_CARD",
+		"FUTURE_TRANSFER",
+		"SCHEDULED_CARD_AMENDMENT",
+	] as const
+
 	export function is(value: unknown): value is StatementReportRowActionType {
-		return typeof value == "string" && statementReportRowActionType.includes(value as StatementReportRowActionType)
+		return typeof value == "string" && values.includes(value as StatementReportRowActionType)
 	}
 	export function toDisplay(value: StatementReportRowActionType): string {
 		if (value == "REFUND_FEE")
--- model/StatementReportSubType.ts
+++ model/StatementReportSubType.ts
@@ -1,15 +1,9 @@
-export const statementReportSubType = [
-	"SETTLED",
-	"PENDING",
-	"PROGNOSIS",
-	"FUTURE_PROGNOSIS",
-	"UNUSED_CARDS_PROGNOSIS",
-] as const
-
-export type StatementReportSubType = typeof statementReportSubType[number]
+export type StatementReportSubType = typeof StatementReportSubType.values[number]
 
 export namespace StatementReportSubType {
+	export const values = ["SETTLED", "PENDING", "PROGNOSIS", "FUTURE_PROGNOSIS", "UNUSED_CARDS_PROGNOSIS"] as const
+
 	export function is(value: unknown): value is StatementReportSubType {
-		return typeof value == "string" && statementReportSubType.includes(value as StatementReportSubType)
+		return typeof value == "string" && values.includes(value as StatementReportSubType)
 	}
 }
