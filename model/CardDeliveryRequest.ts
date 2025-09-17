2a6a86df127037dde3a247745ce86f247a278068
qijieye <qijie.ye@pax2pay.com>
2025-09-17 15:04:45 +0200

    update CardDeliveryRequest

--- model/CardDeliveryRequest.ts
+++ model/CardDeliveryRequest.ts
@@ -5,12 +5,14 @@ export interface CardDeliveryRequest {
 	to: string
 	message: string
 	linkExpiry: isoly.Date
+	attachmentFilename?: string
 }
 export namespace CardDeliveryRequest {
 	export const type = isly.object<CardDeliveryRequest>({
 		to: isly.string(/\S+@\S+\.\S+/),
 		message: isly.string(),
 		linkExpiry: isly.fromIs("isExpiry", isExpiry),
+		attachmentFilename: isly.string().optional(),
 	})
 	function isExpiry(value: any): value is isoly.Date {
 		return isoly.Date.is(value) && value > isoly.Date.now()
