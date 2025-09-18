f092a74e6ca3caff73d03e9252cf61ea8c3b921b
qijieye <qijie.ye@pax2pay.com>
2025-09-18 14:03:01 +0200

    add createWithRemittanceAdvice in payment

--- Client/Payments/index.ts
+++ Client/Payments/index.ts
@@ -14,6 +14,17 @@ export class Payments extends List<model.PaymentResponse> {
 	async create(request: model.PaymentRequest) {
 		return await this.connection.post<model.ErrorResponse | model.PaymentResponse>(this.folder, request)
 	}
+	async createWithRemittanceAdvice(request: model.PaymentRequest, file: File) {
+		const formData = new FormData()
+		formData.append("remittanceAdvice", file, file?.name)
+		formData.append(
+			"request",
+			new Blob([JSON.stringify(request, null, 2)], {
+				type: "application/json",
+			})
+		)
+		return await this.connection.post<model.PaymentResponse>(this.folder, formData)
+	}
 	async createTokenised(request: model.PaymentRequest) {
 		return await this.connection.post<model.ErrorResponse | model.PaymentResponse>(`${this.folder}/tokenised`, request)
 	}
