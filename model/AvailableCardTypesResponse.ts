4857530d9784b1422b6a182e42d0a5dde0e3f8a8
Magnus Gustafsson <magnus.gustafsson@pax2pay.com>
2025-09-22 15:02:46 +0200

    optional cardTypes

--- model/AvailableCardTypesResponse.ts
+++ model/AvailableCardTypesResponse.ts
@@ -2,12 +2,12 @@ import { isly } from "isly"
 import { CardTypeResponse } from "./CardTypeResponse"
 
 export interface AvailableCardTypesResponse {
-	cardTypes: CardTypeResponse[]
+	cardTypes?: CardTypeResponse[]
 }
 
 export namespace AvailableCardTypesResponse {
 	export const type = isly.object<AvailableCardTypesResponse>({
-		cardTypes: isly.array(CardTypeResponse.type),
+		cardTypes: isly.array(CardTypeResponse.type).optional(),
 	})
 	export const is = type.is
 }
