70f3dfee6cdaf1811b9c5b27a970ddb9e84b930f
Magnus Gustafsson <magnus.gustafsson@pax2pay.com>
2025-09-25 12:55:00 +0200

    ui#3258 - Add actionTypes to StatementSummaryRequest

--- model/StatementSummaryReportRequest.ts
+++ model/StatementSummaryReportRequest.ts
@@ -1,12 +1,14 @@
 import * as isoly from "isoly"
 import { ProviderCode } from "./ProviderCode"
 import { BalanceType } from "./Report/BalanceType"
+import { StatementReportRowActionType } from "./StatementReportRowActionType"
 import { StatementReportSubType } from "./StatementReportSubType"
 
 export interface StatementSummaryReportRequest {
 	providerCode: ProviderCode
 	providerAccountId: string
 	balanceType?: BalanceType
+	actionTypes?: StatementReportRowActionType[]
 	dateRange?: {
 		start?: isoly.Date
 		end?: isoly.Date
