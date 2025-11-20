dffbccb4e5e7f7d0aec63cc15fb91923705953de
qijieye <qijie.ye@pax2pay.com>
2025-11-20 11:47:17 +0100

    update

--- model/Report/ReportSubscriptionInvocationResponse.ts
+++ model/Report/ReportSubscriptionInvocationResponse.ts
@@ -3,12 +3,14 @@ import { isly } from "isly"
 import { ReportSubscriptionInvocationOutcome } from "./ReportSubscriptionInvocationOutcome"
 
 export interface ReportSubscriptionInvocationResponse {
+	id?: string
 	createdAt: DateTime
 	outcome: ReportSubscriptionInvocationOutcome
 	errorMessage?: string
 }
 export namespace ReportSubscriptionInvocationResponse {
 	export const type = isly.object<ReportSubscriptionInvocationResponse>({
+		id: isly.string().optional(),
 		createdAt: isly.fromIs("DateTime", DateTime.is),
 		outcome: ReportSubscriptionInvocationOutcome.type,
 		errorMessage: isly.string().optional(),
