efa6b6b709e07ef7d8131c4f0f62333eac046994
Magnus Gustafsson <magnus.gustafsson@pax2pay.com>
2025-11-26 13:20:32 +0100

    paginated batch

--- Client/Batch/index.ts
+++ Client/Batch/index.ts
@@ -1,7 +1,8 @@
 import * as model from "../../model"
 import { Connection } from "../Connection"
 import { List } from "../List"
-export class Batch extends List<model.RolesetResponse> {
+import { Paginated } from "../Paginated"
+export class Batch extends List<model.Batch.Response> {
 	protected readonly folder = "batch"
 	private constructor(connection: Connection) {
 		super(connection)
@@ -14,15 +15,23 @@ export class Batch extends List<model.RolesetResponse> {
 	}
 	async listByType(
 		type: model.Batch.Type,
+		previous?: Paginated<model.Batch.Response>,
+		page?: number,
 		size?: number,
 		sort = "createdOn,desc"
-	): Promise<model.Batch.Response[] | model.ErrorResponse> {
-		const response = await this.connection.get<{ list: model.Batch.Response[]; totalCount: number }>(`${this.folder}`, {
-			type,
+	): Promise<Paginated<model.Batch.Response> | model.ErrorResponse> {
+		return await this.getNextPaginated(
+			previous,
+			(page, size, sort) =>
+				this.connection.get<{ list: model.Batch.Response[]; totalCount: number } | model.Batch.Response[]>(
+					`${this.folder}`,
+					{ type, page, size, sort }
+				),
+			undefined,
+			page,
 			size,
-			sort,
-		})
-		return this.extractResponse<model.Batch.Response>(response)
+			sort
+		)
 	}
 	async run(batchId: string, otp?: string): Promise<model.Batch.Response | model.ErrorResponse> {
 		return await this.connection.get<model.Batch.Response>(
