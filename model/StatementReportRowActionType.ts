636cb7ce5e8c5f440beacd103528253c06b77c83
qijieye <86956109+qijieye@users.noreply.github.com>
2025-11-27 16:07:02 +0100

    Add display values in StatementReportRowActionType (#378)
    * reorder
    
    * update
    
    * update
    
    * wip

--- model/StatementReportRowActionType.ts
+++ model/StatementReportRowActionType.ts
@@ -2,21 +2,26 @@ export type StatementReportRowActionType = typeof StatementReportRowActionType.v
 
 export namespace StatementReportRowActionType {
 	export const values = [
+		"TRANSFER_IN",
+		"TRANSFER_OUT",
+		"REFUND",
 		"AUTHORISATION",
 		"SETTLEMENT",
 		"REVERSAL",
-		"REFUND",
 		"AUTHORISATION_FEE",
 		"SETTLEMENT_FEE",
 		"REVERSAL_FEE",
 		"REFUND_FEE",
-		"TRANSFER_IN",
-		"TRANSFER_OUT",
 		"USABLE_CARD",
 		"FUTURE_TRANSFER",
 		"SCHEDULED_CARD_AMENDMENT",
 	] as const
-
+	export const displayValues = [
+		["TRANSFER_IN", "TRANSFER_OUT"],
+		["REFUND", "AUTHORISATION", "SETTLEMENT", "REVERSAL"],
+		["FEE"],
+		["USABLE_CARD", "FUTURE_TRANSFER", "SCHEDULED_CARD_AMENDMENT"],
+	] as const
 	export function is(value: unknown): value is StatementReportRowActionType {
 		return typeof value == "string" && values.includes(value as StatementReportRowActionType)
 	}
