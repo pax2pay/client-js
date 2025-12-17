549cd0865c43c88c761cec66068072e72a101d1b
qijieye <qijie.ye@pax2pay.com>
2025-12-17 13:45:43 +0100

    fix FundingLimit.is()

--- model/Config/Notice/FundingLimit/index.ts
+++ model/Config/Notice/FundingLimit/index.ts
@@ -4,13 +4,13 @@ import { Configuration as FConfiguration } from "./Configuration"
 export interface FundingLimit {
 	accountId: string
 	threshold: number
-	currency?: Currency
+	currency: Currency
 }
 export namespace FundingLimit {
 	export const type = isly.object<FundingLimit>({
 		accountId: isly.string(),
 		threshold: isly.number(),
-		currency: isly.string<Currency>().optional(),
+		currency: isly.string<Currency>(),
 	})
 	export const is = type.is
 	export import Configuration = FConfiguration
