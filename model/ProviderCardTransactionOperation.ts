e95e862882a83fed6da6d4436f08e29cd5c28b23
qijieye <qijie.ye@pax2pay.com>
2026-02-05 15:35:36 +0100

    add transactionId

--- model/ProviderCardTransactionOperation.ts
+++ model/ProviderCardTransactionOperation.ts
@@ -1,12 +1,15 @@
+import { isly } from "isly"
 import { AbstractProviderTransactionOperation } from "./AbstractProviderTransactionOperation"
 import { MerchantDetailsV2 } from "./MerchantDetailsV2"
 
 export interface ProviderCardTransactionOperation extends AbstractProviderTransactionOperation {
 	merchant?: MerchantDetailsV2
+	providerTransactionId?: string
 }
 export namespace ProviderCardTransactionOperation {
 	export const type = AbstractProviderTransactionOperation.type.extend<ProviderCardTransactionOperation>({
 		merchant: MerchantDetailsV2.type.optional(),
+		providerTransactionId: isly.string().optional(),
 	})
 	export const is = type.is
 }
