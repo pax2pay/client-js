47b33d73babc08104551be68d83ec4e620f16491
qijieye <qijie.ye@pax2pay.com>
2026-03-05 14:26:36 +0100

    add verificationOfPayeeId in PaymentTransferCreateRequest

--- model/PaymentTransferCreateRequest.ts
+++ model/PaymentTransferCreateRequest.ts
@@ -3,11 +3,13 @@ import { isly } from "isly"
 export interface PaymentTransferCreateRequest {
 	beneficiary?: string
 	reference?: string
+	verificationOfPayeeId?: string
 }
 export namespace PaymentTransferCreateRequest {
 	export const type = isly.object<PaymentTransferCreateRequest>({
 		beneficiary: isly.string().optional(),
 		reference: isly.string().optional(),
+		verificationOfPayeeId: isly.string().optional(),
 	})
 	export const is = type.is
 }
