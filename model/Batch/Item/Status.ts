f025ba55ab54498bcfe543c90badc4e285f762aa
qijieye <qijie.ye@pax2pay.com>
2026-03-18 11:32:04 +0100

    fix

--- model/Batch/Item/Status.ts
+++ model/Batch/Item/Status.ts
@@ -3,6 +3,6 @@ import { isly } from "isly"
 export type Status = typeof Status.values[number]
 
 export namespace Status {
-	export const values = ["todo", "successful", "failed", "canceled", "failed import", "importing"] as const
+	export const values = ["todo", "successful", "failed", "canceled", "failed import"] as const
 	export const type = isly.string(values)
 }
