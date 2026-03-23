63c8b35434a2b3a283c9a47872c2e9377156344c
qijieye <86956109+qijieye@users.noreply.github.com>
2026-03-23 16:32:27 +0100

    card type can be string (#409)
    * card type can be string
    
    * add more status
    
    * add more status

--- model/Batch/Status.ts
+++ model/Batch/Status.ts
@@ -3,7 +3,17 @@ import { isly } from "isly"
 export type Status = typeof Status.values[number]
 
 export namespace Status {
-	export const values = ["todo", "processing", "completed", "failed to finalise", "cancelled", "importing"] as const
+	export const values = [
+		"todo",
+		"processing",
+		"completed",
+		"failed to finalise",
+		"cancelled",
+		"importing",
+		"finalising",
+		"finalising import",
+		"failed to import",
+	] as const
 	export const type = isly.string(values)
 	export const is = type.is
 }
