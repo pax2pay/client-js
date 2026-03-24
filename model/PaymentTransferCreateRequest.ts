7bba4f59dbabc67458ec15bf1d2428c181bd66d5
qijieye <qijie.ye@pax2pay.com>
2026-03-24 13:18:43 +0100

    add beneficiaryName in PaymentTransferCreateRequest

--- model/PaymentTransferCreateRequest.ts
+++ model/PaymentTransferCreateRequest.ts
@@ -2,12 +2,14 @@ import { isly } from "isly"
 
 export interface PaymentTransferCreateRequest {
 	beneficiary?: string
+	beneficiaryName?: string
 	reference?: string
 	verificationOfPayeeId?: string
 }
 export namespace PaymentTransferCreateRequest {
 	export const type = isly.object<PaymentTransferCreateRequest>({
 		beneficiary: isly.string().optional(),
+		beneficiaryName: isly.string().optional(),
 		reference: isly.string().optional(),
 		verificationOfPayeeId: isly.string().optional(),
 	})
