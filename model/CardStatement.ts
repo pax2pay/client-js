88e32bdc32a05bbbd315f3a10c2580c0ebd5d43e
ChriJ <christofferj93@gmail.com>
2026-04-07 10:48:18 +0200

    mpay-server#3479 - Card operations displays billing amount wrong - client (#420)
    * mpay-server#3479 - Card operations displays billing amount wrong - client
    
    * review
    
    ---------
    
    Co-authored-by: Christoffer Johansson <christoffer.johansson@pax2pay.com>

--- model/CardStatement.ts
+++ model/CardStatement.ts
@@ -9,10 +9,10 @@ export interface CardStatement {
 	description: string
 	type: string
 	status?: string
-	balance: number
+	transactionBalance: number
 	billingBalance: number
 	fundsChanged?: number
-	currency?: isoly.Currency
+	transactionCurrency?: isoly.Currency
 	billingCurrency?: isoly.Currency
 	fxRate?: number
 	reason?: string
@@ -28,10 +28,10 @@ export namespace CardStatement {
 		description: isly.string(),
 		type: isly.string(),
 		status: isly.string().optional(),
-		balance: isly.number(),
+		transactionBalance: isly.number(),
 		billingBalance: isly.number(),
 		fundsChanged: isly.number().optional(),
-		currency: isly.fromIs("isoly.Currency", isoly.Currency.is).optional(),
+		transactionCurrency: isly.fromIs("isoly.Currency", isoly.Currency.is).optional(),
 		billingCurrency: isly.fromIs("isoly.Currency", isoly.Currency.is).optional(),
 		fxRate: isly.number().optional(),
 		reason: isly.string().optional(),
