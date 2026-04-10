f9470bb6a8ffb1ba4a557363d875055cbfa4542a
Lucy Meachem <lucy.meachem@paxport.net>
2026-04-10 12:40:56 +0100

    Add DownloadFileFormat#CAMT_053

--- model/DownloadFileFormat.ts
+++ model/DownloadFileFormat.ts
@@ -3,7 +3,7 @@ import { isly } from "isly"
 export type DownloadFileFormat = (typeof DownloadFileFormat.values)[number]
 
 export namespace DownloadFileFormat {
-	export const values = ["CSV", "PDF", "HTML"] as const
+	export const values = ["CSV", "PDF", "HTML", "CAMT_053"] as const
 	export const type = isly.string(values)
 	export const is = type.is
 }
