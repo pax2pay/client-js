d10d5bc22433dc4df2adb0d1fe1e869b6b2ec966
qijieye <86956109+qijieye@users.noreply.github.com>
2026-06-09 17:01:46 +0200

    add batch payment xlsx template download endpoint (#437)
    * add batch payment xlsx template download endpoint
    
    * fix
    
    * revert the interface change

--- Client/Batch/index.ts
+++ Client/Batch/index.ts
@@ -67,4 +67,7 @@ export class Batch extends List<model.Batch.Response> {
 			sort
 		)
 	}
+	async downloadBatchPaymentTemplate(): Promise<model.DownloadableResponse | model.ErrorResponse> {
+		return await this.connection.get<model.DownloadableResponse>(`${this.folder}/payment/template`)
+	}
 }
