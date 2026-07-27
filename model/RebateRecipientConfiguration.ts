cda4cece70c5d77b816979dfee84385e2e1551df
qijieye <qijie.ye@pax2pay.com>
2026-07-27 13:54:26 +0200

    fields are all optional in rebate recipient

--- model/RebateRecipientConfiguration.ts
+++ model/RebateRecipientConfiguration.ts
@@ -2,18 +2,18 @@ import { isly } from "isly"
 import { AddressInfo } from "./AddressInfo"
 
 export interface RebateRecipientConfiguration {
-	enabled: boolean
-	address: AddressInfo
-	qualifiedAddresses: Record<string, AddressInfo>
-	qualifiers: string[]
+	enabled?: boolean
+	address?: AddressInfo
+	qualifiedAddresses?: Record<string, AddressInfo>
+	qualifiers?: string[]
 }
 
 export namespace RebateRecipientConfiguration {
 	export const type = isly.object<RebateRecipientConfiguration>({
-		enabled: isly.boolean(),
-		address: AddressInfo.type,
-		qualifiedAddresses: isly.record(isly.string(), AddressInfo.type),
-		qualifiers: isly.string().array(),
+		enabled: isly.boolean().optional(),
+		address: AddressInfo.type.optional(),
+		qualifiedAddresses: isly.record<Record<string, AddressInfo>>(isly.string(), AddressInfo.type).optional(),
+		qualifiers: isly.string().array().optional(),
 	})
 	export const is = type.is
 }
